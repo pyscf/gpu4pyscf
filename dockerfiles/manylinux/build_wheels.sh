df70a657d6fb44ef5a884ce7491bbcdf1ebc9fd5
xiaojie.wu <xiaojie.wu@bytedance.com>
2024-01-17 05:20:39 +0800

    updated the script for building wheels

--- dockerfiles/manylinux/build_wheels.sh
+++ dockerfiles/manylinux/build_wheels.sh
@@ -13,6 +13,8 @@ export CUDA_HOME=/usr/local/cuda
 export CUTENSOR_DIR=/usr/local/cuda
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
+# blas is required by DFTD3 and DFTD4
 yum install openblas-devel
 
 # Compile wheels
