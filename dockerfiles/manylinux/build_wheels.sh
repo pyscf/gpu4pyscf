ae22569d73307dfdd4848d9f1f160925e169906f
xiaojie.wu <xiaojie.wu@bytedance.com>
2024-01-17 04:28:02 +0800

    resolve dependencies in dftd3 and dftd4

--- dockerfiles/manylinux/build_wheels.sh
+++ dockerfiles/manylinux/build_wheels.sh
@@ -13,6 +13,7 @@ export CUDA_HOME=/usr/local/cuda
 export CUTENSOR_DIR=/usr/local/cuda
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+yum install openblas-devel
 
 # Compile wheels
 rm -rf /gpu4pyscf/wheelhouse
