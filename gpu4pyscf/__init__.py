cff091b151cd04a22d83954332405078e04d92a4
xiaojie.wu <xiaojie.wu@bytedance.com>
2024-01-31 02:59:56 +0800

    remove dftd3; bump up to 0.7.0

--- gpu4pyscf/__init__.py
+++ gpu4pyscf/__init__.py
@@ -1,6 +1,6 @@
 from . import lib, grad, hessian, solvent, scf, dft
 
-__version__ = '0.6.17'
+__version__ = '0.7.0'
 
 # monkey patch libxc reference due to a bug in nvcc
 from pyscf.dft import libxc
