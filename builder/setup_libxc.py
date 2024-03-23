303140d221c4b53085b2c44da4cb06fd009802a2
Xiaojie Wu <wxj6000@gmail.com>
2024-03-22 17:20:21 -0700

    Update setup_libxc.py

--- builder/setup_libxc.py
+++ builder/setup_libxc.py
@@ -35,7 +35,7 @@ URL = None
 DOWNLOAD_URL = None
 CLASSIFIERS = None
 PLATFORMS = None
-VERSION = '0.3'
+VERSION = '0.4'
 
 def get_cuda_version():
     nvcc_out = subprocess.check_output(["nvcc", "--version"]).decode('utf-8')
