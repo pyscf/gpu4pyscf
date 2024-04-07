4189bfc1dbdd0a84a84e25d10f94a3504dfc8e70
xiaojie.wu <xiaojie.wu@bytedance.com>
2024-04-07 15:00:15 +0800

    skip rhf grad

--- gpu4pyscf/grad/tests/test_uhf_grad.py
+++ gpu4pyscf/grad/tests/test_uhf_grad.py
@@ -17,12 +17,15 @@ import pyscf
 import numpy as np
 import unittest
 from gpu4pyscf import scf
+from packaging import version
 
 atom = '''
 O       0.0000000000    -0.0000000000     0.1174000000
 H      -0.7570000000    -0.0000000000    -0.4696000000
 H       0.7570000000     0.0000000000    -0.4696000000
 '''
+
+pyscf_25 = version.parse(pyscf.__version__) <= version.parse('2.5.0')
 
 bas0='cc-pvtz'
 
@@ -67,10 +70,12 @@ class KnownValues(unittest.TestCase):
         print('---- testing UHF Cart -------')
         _check_grad(mol_cart, tol=1e-6)
 
+    @pytest.mark.skipif(pyscf_25, reason='requires pyscf 2.6 or higher')
     def test_grad_d3bj(self):
         print('---- testing UHF with D3(BJ) ----')
         _check_grad(mol_sph, tol=1e-6, disp='d3bj')
 
+    @pytest.mark.skipif(pyscf_25, reason='requires pyscf 2.6 or higher')
     def test_grad_d4(self):
         print('------- UHF with D4 -----')
         _check_grad(mol_sph, tol=1e-6, disp='d4')
