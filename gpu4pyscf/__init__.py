f49d27b34254c5f3536416e0e38e22ff0f10962e
Xiaojie Wu <wxj6000@gmail.com>
2024-05-29 01:53:37 +0000

    release v0.7.8

--- gpu4pyscf/__init__.py
+++ gpu4pyscf/__init__.py
@@ -1,6 +1,6 @@
 from . import lib, grad, hessian, solvent, scf, dft
 
-__version__ = '0.7.7'
+__version__ = '0.7.8'
 
 # monkey patch libxc reference due to a bug in nvcc
 from pyscf.dft import libxc
