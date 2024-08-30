0c0491e57769ae77c4fa3ec16a15a684951c5122
xiaojie.wu <xiaojie.wu@bytedance.com>
2024-08-31 02:25:39 +0800

    fixed a bug when saving electron density with spin unrestricted

--- gpu4pyscf/drivers/dft_driver.py
+++ gpu4pyscf/drivers/dft_driver.py
@@ -190,7 +190,8 @@ def run_dft(mol_name, config, charge=None, spin=0):
         if save_density and xc.lower() != 'hf':
             weights = mf.grids.weights
             coords = mf.grids.coords
-            rho = mf._numint.get_rho(mf.mol, dm, mf.grids)
+            dm0 = dm[0] + dm[1] if dm.ndim == 3 else dm
+            rho = mf._numint.get_rho(mf.mol, dm0, mf.grids)
 
             if isinstance(weights, cupy.ndarray): weights = weights.get()
             if isinstance(coords, cupy.ndarray):  coords  = coords.get()
