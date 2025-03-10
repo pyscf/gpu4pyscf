b02f356f8e245f465c62ffe6d85ecfd9207773b2
Xiaojie Wu <wxj6000@gmail.com>
2025-03-10 14:49:11 -0700

    Update version

--- gpu4pyscf/__init__.py
+++ gpu4pyscf/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 
 from . import lib, grad, hessian, solvent, scf, dft
