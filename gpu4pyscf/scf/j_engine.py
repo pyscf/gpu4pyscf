53bfa5cc83edf568cec673e0f567e30c03136e58
Qiming Sun <qiming.sun@bytedance.com>
2025-04-23 06:40:16 +0800

    s4 symmetry for md-j engine

--- gpu4pyscf/scf/j_engine.py
+++ gpu4pyscf/scf/j_engine.py
@@ -205,12 +205,18 @@ def _md_j_engine_quartets_scheme(ls, shm_size=SHM_SIZE):
 
     tilex = min(32, 128 // (lij+1))
     tiley = min(32, 128 // (lkl+1))
-    cache_size = ij * 4 + kl*tiley * 4 + ij * nf3ij * 2 + kl * nf3kl * 2
+    if li == lk and lj == ll:
+        cache_size = ij * 4 + kl*tiley * 4 + ij * nf3ij + kl * nf3kl
+    else:
+        cache_size = ij * 4 + kl*tiley * 4 + ij * nf3ij * 2 + kl * nf3kl * 2
     while (nsq * unit + cache_size) * 8 > shm_size:
         nsq //= 2
         kl = _nearest_power2(int(nsq**.5))
         ij = nsq // kl
-        cache_size = ij * 4 + kl*tiley * 4 + ij * nf3ij * 2 + kl * nf3kl * 2
+        if li == lk and lj == ll:
+            cache_size = ij * 4 + kl*tiley * 4 + ij * nf3ij + kl * nf3kl
+        else:
+            cache_size = ij * 4 + kl*tiley * 4 + ij * nf3ij * 2 + kl * nf3kl * 2
     gout_stride = threads // nsq
 
     # Adjust tiley, to effectively utilize the 40 registers per thread as cache
