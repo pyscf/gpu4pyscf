c6649ec35515cc7c5f080f014c7dc7d083546d95
puzhichen.996 <puzhichen.996@bytedance.com>
2025-04-24 09:00:52 +0800

    in writting

--- gpu4pyscf/nac/tdrhf.py
+++ gpu4pyscf/nac/tdrhf.py
@@ -24,5 +24,24 @@ from pyscf import __config__
 from gpu4pyscf.lib import utils
 from gpu4pyscf import tdscf
 
+
+def get_densities(td_nac, mf, x_yi, x_yj, singlet=True, relaxed=True):
+    if singlet is None:
+        singlet = True
+    log = logger.new_logger(td_grad, verbose)
+    time0 = logger.init_timer(td_grad)
+    mol = td_grad.mol
+    mf = td_grad.base._scf
+    mo_coeff = cp.asarray(mf.mo_coeff)
+    mo_energy = cp.asarray(mf.mo_energy)
+    mo_occ = cp.asarray(mf.mo_occ)
+    nmo = mo_coeff.shape[1]
+    nocc = int((mo_occ > 0).sum())
+    nvir = nmo - nocc
+    x, y = x_y
+    x = cp.asarray(x)
+    y = cp.asarray(y)
+
+
 def get_nacv(td_nac, x_y, singlet=True, atmlst=None, verbose=logger.INFO):
     pass
