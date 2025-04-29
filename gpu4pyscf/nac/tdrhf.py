68d1c6c5cc4356a974194c22b75a69ec17df9d25
puzhichen.996 <puzhichen.996@bytedance.com>
2025-04-29 11:34:15 +0800

    in writting

--- gpu4pyscf/nac/tdrhf.py
+++ gpu4pyscf/nac/tdrhf.py
@@ -67,199 +67,86 @@ def get_nacv(td_nac, x_yI, x_yJ, EI, EJ, singlet=True, atmlst=None, verbose=logg
     if yJ == 0:
         yJ = xJ * 0.0
     yJ = cp.asarray(yJ).reshape(nocc, nvir).T
-    RxI = reduce(cp.dot, (orbv, xI, orbo.T)) * 2.0
-    RyI = reduce(cp.dot, (orbv, yI, orbo.T)) * 2.0
-    RxJ = reduce(cp.dot, (orbv, xJ, orbo.T)) * 2.0
-    RyJ = reduce(cp.dot, (orbv, yJ, orbo.T)) * 2.0
-    DIJ_mo = 2*cp.dot(xI,xJ.T)
-    DIJ_mo+= 2*cp.dot(yJ,yI.T)
-    DIJ = reduce(cp.dot, (orbv, DIJ_mo, orbv.T))
-    DIJ_mo = 2*cp.dot(xJ.T,xI)
-    DIJ_mo+= 2*cp.dot(yI.T,yJ)
-    DIJ -= reduce(cp.dot, (orbo, DIJ_mo, orbo.T))
-    
-    vjxI1, vkxI1 = mf.get_jk(mol, RxI, hermi=0)    #nu sigma in article
-    vjxI2, vkxI2 = mf.get_jk(mol, RxI.T, hermi=0)
-    vjyI1, vkyI1 = mf.get_jk(mol, RyI, hermi=0)
-    vjyI2, vkyI2 = mf.get_jk(mol, RyI.T, hermi=0)
-    vjxJ1, vkxJ1 = mf.get_jk(mol, RxJ, hermi=0)
-    vjxJ2, vkxJ2 = mf.get_jk(mol, RxJ.T, hermi=0)
-    vjyJ1, vkyJ1 = mf.get_jk(mol, RyJ, hermi=0)
-    vjyJ2, vkyJ2 = mf.get_jk(mol, RyJ.T, hermi=0)
-    vjDIJ, vkDIJ = mf.get_jk(mol, DIJ, hermi=0)
-    if not isinstance(vjxI1, cp.ndarray):
-        vjxI1 = cp.asarray(vjxI1)
-        vkxI1 = cp.asarray(vkxI1)
-        vjxI2 = cp.asarray(vjxI2)
-        vkxI2 = cp.asarray(vkxI2)
-        vjyI1 = cp.asarray(vjyI1)
-        vkyI1 = cp.asarray(vkyI1)
-        vjyI2 = cp.asarray(vjyI2)
-        vkyI2 = cp.asarray(vkyI2)
-        vjxJ1 = cp.asarray(vjxJ1)
-        vkxJ1 = cp.asarray(vkxJ1)
-        vjxJ2 = cp.asarray(vjxJ2)
-        vkxJ2 = cp.asarray(vkxJ2)
-        vjyJ1 = cp.asarray(vjyJ1)
-        vkyJ1 = cp.asarray(vkyJ1)
-        vjyJ2 = cp.asarray(vjyJ2)
-        vkyJ2 = cp.asarray(vkyJ2)
-        vjDIJ = cp.asarray(vjDIJ)
-        vkDIJ = cp.asarray(vkDIJ)
-    veffxI1 = 2 * vjxI1 - vkxI1
-    veffxI2 = 2 * vjxI2 - vkxI2
-    veffyI1 = 2 * vjyI1 - vkyI1
-    veffyI2 = 2 * vjyI2 - vkyI2
-    veffxJ1 = 2 * vjxJ1 - vkxJ1
-    veffxJ2 = 2 * vjxJ2 - vkxJ2
-    veffyJ1 = 2 * vjyJ1 - vkyJ1
-    veffyJ2 = 2 * vjyJ2 - vkyJ2
-    veffDIJ = 2 * vjDIJ - vkDIJ
-    veffxI1_mo = reduce(cp.dot, (mo_coeff.T, veffxI1, mo_coeff))
-    veffxI2_mo = reduce(cp.dot, (mo_coeff.T, veffxI2, mo_coeff))
-    veffyI1_mo = reduce(cp.dot, (mo_coeff.T, veffyI1, mo_coeff))
-    veffyI2_mo = reduce(cp.dot, (mo_coeff.T, veffyI2, mo_coeff))
-    veffxJ1_mo = reduce(cp.dot, (mo_coeff.T, veffxJ1, mo_coeff))
-    veffxJ2_mo = reduce(cp.dot, (mo_coeff.T, veffxJ2, mo_coeff))
-    veffyJ1_mo = reduce(cp.dot, (mo_coeff.T, veffyJ1, mo_coeff))
-    veffyJ2_mo = reduce(cp.dot, (mo_coeff.T, veffyJ2, mo_coeff))
-    # first term of L
-    # wvo = reduce(cp.dot, (orbv.T, veffxI1, orbv, xJ)) * 2.0
-    # wvo+= reduce(cp.dot, (orbv.T, veffyI1, orbv, yJ)) * 2.0
-    # wvo+= reduce(cp.dot, (orbv.T, veffxJ1, orbv, xI)) * 2.0
-    # wvo+= reduce(cp.dot, (orbv.T, veffyJ1, orbv, yI)) * 2.0
-    # wvo+= reduce(cp.dot, (orbv.T, veffxI2, orbv, yJ)) * 2.0
-    # wvo+= reduce(cp.dot, (orbv.T, veffyI2, orbv, xJ)) * 2.0
-    # wvo+= reduce(cp.dot, (orbv.T, veffxJ2, orbv, yI)) * 2.0
-    # wvo+= reduce(cp.dot, (orbv.T, veffyJ2, orbv, xI)) * 2.0
-    wvo = veffxI1_mo[nocc:, nocc:]@xJ
-    wvo+= veffyI1_mo[nocc:, nocc:]@yJ
-    wvo+= veffxJ1_mo[nocc:, nocc:]@xI
-    wvo+= veffyJ1_mo[nocc:, nocc:]@yI
-    wvo+= veffxI2_mo[nocc:, nocc:]@yJ
-    wvo+= veffyI2_mo[nocc:, nocc:]@xJ
-    wvo+= veffxJ2_mo[nocc:, nocc:]@yI
-    wvo+= veffyJ2_mo[nocc:, nocc:]@xI
-    # second term of L
-    # wvo-= reduce(cp.dot, (xJ, orbo.T, veffxI1, orbo)) * 2.0
-    # wvo-= reduce(cp.dot, (yJ, orbo.T, veffyI1, orbo)) * 2.0
-    # wvo-= reduce(cp.dot, (xI, orbo.T, veffxJ1, orbo)) * 2.0
-    # wvo-= reduce(cp.dot, (yI, orbo.T, veffyJ1, orbo)) * 2.0
-    # wvo-= reduce(cp.dot, (yJ, orbo.T, veffxI2, orbo)) * 2.0
-    # wvo-= reduce(cp.dot, (xJ, orbo.T, veffyI2, orbo)) * 2.0
-    # wvo-= reduce(cp.dot, (yI, orbo.T, veffxJ2, orbo)) * 2.0
-    # wvo-= reduce(cp.dot, (xI, orbo.T, veffyJ2, orbo)) * 2.0
-    wvo -= xJ@veffxI1_mo[:nocc, :nocc]
-    wvo -= yJ@veffyI1_mo[:nocc, :nocc]
-    wvo -= xI@veffxJ1_mo[:nocc, :nocc]
-    wvo -= yI@veffyJ1_mo[:nocc, :nocc]
-    wvo -= yJ@veffxI2_mo[:nocc, :nocc]
-    wvo -= xJ@veffyI2_mo[:nocc, :nocc]
-    wvo -= yI@veffxJ2_mo[:nocc, :nocc]
-    wvo -= xI@veffyJ2_mo[:nocc, :nocc]
-    # forth term of L
-    wvo+= reduce(cp.dot, (orbv.T, veffDIJ, orbo))
-    # wvo *= 2.0
+    LI = reduce(cp.dot, (orbv, xI-yI, orbo.T)) * 2.0
+    dmxpyI = reduce(cp.dot, (orbv, xIyI, orbo.T))
 
-    vresp = td_nac.base._scf.gen_response(singlet=None, hermi=0)
+    vresp = mf.gen_response(singlet=None, hermi=1)
+
     def fvind(x):
         dm = reduce(cp.dot, (orbv, x.reshape(nvir, nocc) * 2, orbo.T))
-        v1ao = vresp(dm)
+        v1ao = vresp(dm + dm.T)
         return reduce(cp.dot, (orbv.T, v1ao, orbo)).ravel()
+
+    def contract_G(Zs):
+        vj, vk = mf.get_jk(mol, Zs, hermi=0)
+        if not isinstance(vj, cp.ndarray): vj = cp.asarray(vj)
+        if not isinstance(vk, cp.ndarray): vk = cp.asarray(vk)
+        return 2*vj - vk
+
     z1 = cphf.solve(
         fvind,
         mo_energy,
         mo_occ,
-        wvo,
+        LI*2.0,
         max_cycle=td_nac.cphf_max_cycle,
         tol=td_nac.cphf_conv_tol)[0]
     z1 = z1.reshape(nvir, nocc)
-    z1ao = 2*reduce(cp.dot, (orbv, z1, orbo.T))
+    z1ao = reduce(cp.dot, (orbv, z1, orbo.T))
+    z1aoS = z1ao + z1ao.T
+    GZS = contract_G(z1aoS)
+    GZS_mo = reduce(cp.dot, (mo_coeff.T, GZS[0], mo_coeff))
+    W = cp.zeros((nmo, nmo))
+    W[:nocc, :nocc] = GZS_mo[:nocc, :nocc]
+    W[nocc:, nocc:] = 0.5*yI
+    zeta = mo_energy[nocc:, cp.newaxis]
+    zeta*=z1
+    W[:nocc, nocc:] = GZS_mo[:nocc, nocc:] + 0.5*xI.T + zeta
+    zeta = mo_energy[cp.newaxis, :nocc]
+    zeta*=z1
+    W[nocc:, :nocc] = 0.5*yI + zeta
 
-    DIJtilde = DIJ - z1ao - z1ao.T # .T is included in z1ao
-    P = mf.make_rdm1()
-    Ptilde = P + cp.dot(orbv, orbv.T)*2.0
-    fmat = mf.get_fock()
-    h1 = cp.asarray(mf_grad.get_hcore(mol))  # without 1/r like terms
-    s1 = cp.asarray(mf_grad.get_ovlp(mol))
+    mf_grad = mf._scf.nuc_grad_method()
+    s1 = mf_grad.get_ovlp(mol)
 
-    de1 = contract("xij,ij->xi", h1, (DIJtilde+DIJtilde.T)*0.5)
-    de1_r = int3c2e.get_dh1e(mol, (DIJtilde+DIJtilde.T)*0.5)  # 1/r like terms
-    if mol.has_ecp():
-        de1_r  = rhf_grad.get_dh1e_ecp(mol, (DIJtilde+DIJtilde.T)*0.5)  # 1/r like terms
-
-    Ds = reduce(cp.dot, (Ptilde, fmat, DIJtilde+DIJtilde.T))
-    des1 = contract("xij,ij->xi", s1, Ds)*0.5
-
-    Ds2 = reduce(cp.dot, (Ptilde, veffxJ2, RxI.T))
-    Ds2+= reduce(cp.dot, (Ptilde, veffyJ2, RyI.T))
-    Ds2+= reduce(cp.dot, (Ptilde, veffxI2, RxJ.T))
-    Ds2+= reduce(cp.dot, (Ptilde, veffyI2, RyJ.T))
-    Ds2+= reduce(cp.dot, (Ptilde, veffyJ1, RxI.T))
-    Ds2+= reduce(cp.dot, (Ptilde, veffxJ1, RyI.T))
-    Ds2+= reduce(cp.dot, (Ptilde, veffyI1, RxJ.T))
-    Ds2+= reduce(cp.dot, (Ptilde, veffxI1, RyJ.T))
-    des2 = contract("xij,ij->xi", s1, Ds2)*0.5
-
-    Ds3 = reduce(cp.dot, (Ptilde, veffxJ1, RxI))
-    Ds3+= reduce(cp.dot, (Ptilde, veffyJ1, RyI))
-    Ds3+= reduce(cp.dot, (Ptilde, veffxI1, RxJ))
-    Ds3+= reduce(cp.dot, (Ptilde, veffyI1, RyJ))
-    Ds3+= reduce(cp.dot, (Ptilde, veffyJ2, RxI))
-    Ds3+= reduce(cp.dot, (Ptilde, veffxJ2, RyI))
-    Ds3+= reduce(cp.dot, (Ptilde, veffyI2, RxJ))
-    Ds3+= reduce(cp.dot, (Ptilde, veffxI2, RyJ))
-    des3 = contract("xij,ij->xi", s1, Ds3)*0.5
-    
-    Ds5 = reduce(cp.dot, (Ptilde, veffDIJ, P.T))
-    des5 = contract("xij,ij->xi", s1, Ds5)*0.5
-    
-    delec = de1 - des1 - des2 - des3 + des5
-    # delec = des5
-    aoslices = mol.aoslice_by_atom()
-    delec = cp.asarray([cp.sum(delec[:, p0:p1], axis=1) for p0, p1 in aoslices[:, 2:]])
-    de = (delec)/(EJ - EI)
+    dmz1doo = z1ao  # P
+    oo0 = reduce(cp.dot, (orbo, orbo.T))  # D
 
     if atmlst is None:
         atmlst = range(mol.natm)
-    offsetdic = mol.offset_nr_by_atom()
-    s1int = mol.intor('int1e_ipovlp', comp=3)
-    s1int = cp.asarray(s1int)
-    xx = 2 * cp.dot(xI,xJ.T)
-    yy = 2 * cp.dot(yI,yJ.T)
-    DS6 = reduce(cp.dot, (orbv, xx-yy, orbv.T))
-    xx = 2 * cp.dot(xI.T,xJ)
-    yy = 2 * cp.dot(yI.T,yJ)
-    DS6+= reduce(cp.dot, (orbo, xx-yy, orbo.T))
 
-    eri1 = mol.intor('int2e_ip1', aosym='s1', comp=3)
-    eri1 = cp.asarray(eri1)
-    eri1 =-eri1.reshape(3,nao,nao,nao,nao)
-    
+    h1 = cp.asarray(mf_grad.get_hcore(mol))  # without 1/r like terms
+    s1 = cp.asarray(mf_grad.get_ovlp(mol))
+    dh_ground = contract("xij,ij->xi", h1, oo0 * 2)
+    dh_td = contract("xij,ij->xi", h1, (dmz1doo + dmz1doo.T) * 0.5)
+    ds = contract("xij,ij->xi", s1, (W + W.T) * 0.5)
+    ds2 = contract("xij,ji->xj", s1, )
+
+    dh1e_ground = int3c2e.get_dh1e(mol, oo0 * 2)  # 1/r like terms
+    if mol.has_ecp():
+        dh1e_ground += rhf_grad.get_dh1e_ecp(mol, oo0 * 2)  # 1/r like terms
+    dh1e_td = int3c2e.get_dh1e(mol, (dmz1doo + dmz1doo.T) * 0.5)  # 1/r like terms
+    if mol.has_ecp():
+        dh1e_td += rhf_grad.get_dh1e_ecp(mol, (dmz1doo + dmz1doo.T) * 0.5)  # 1/r like terms
+    extra_force = cp.zeros((len(atmlst), 3))
+
+    dvhf_all = 0
+    # this term contributes the ground state contribution.
+    dvhf = td_nac.get_veff(mol, (dmz1doo + dmz1doo.T) * 0.5 + oo0 * 2) 
     for k, ia in enumerate(atmlst):
-        shl0, shl1, p0, p1 = offsetdic[ia]
-        SA = s1int.copy()
-        SA[:, :p0] = 0
-        SA[:, p1:] = 0
-        Sq = SA+SA.transpose(0,2,1)
-        SA = 0.5*Sq
-        de[k] -= cp.einsum("xij,ji->x", SA[:, p0:p1], DS6[:, p0:p1])
-        de[k] -= cp.einsum("xji,ji->x", SA[:, :, p0:p1], DS6[:, p0:p1])
+        extra_force[k] += mf_grad.extra_force(ia, locals())
+    dvhf_all += dvhf
+    # this term will remove the unused-part from PP density.
+    dvhf = td_nac.get_veff(mol, (dmz1doo + dmz1doo.T) * 0.5)
+    for k, ia in enumerate(atmlst):
+        extra_force[k] -= mf_grad.extra_force(ia, locals())
+    dvhf_all -= dvhf
 
-        eri1a = eri1.copy()
-        eri1a[:,:p0] = 0
-        eri1a[:,p1:] = 0
-        eri1a = eri1a + eri1a.transpose(0,2,1,3,4)
-        eri1a = eri1a + eri1a.transpose(0,3,4,1,2)
-        veff1 = eri1a * 2 - eri1a.transpose(0,1,4,3,2)
-        de_vjk1 = cp.einsum('xuvls,ul,sv->x', veff1, RxI, RxJ)
-        de_vjk1+= cp.einsum('xuvls,ul,sv->x', veff1, RyI, RyJ)
-        de_vjk1+= cp.einsum('xuvls,ul,vs->x', veff1, RxI, RyJ)
-        de_vjk1+= cp.einsum('xuvls,ul,vs->x', veff1, RyI, RxJ)
-        de_vjk1+= cp.einsum('xuvls,ul,sv->x', veff1, DIJtilde, P)
-        de[k] += de_vjk1/(EJ - EI)
+    delec = 2.0 * (dh_ground + dh_td - ds)
+    aoslices = mol.aoslice_by_atom()
+    delec = cp.asarray([cp.sum(delec[:, p0:p1], axis=1) for p0, p1 in aoslices[:, 2:]])
+    de = 2.0 * dvhf_all + dh1e_ground + dh1e_td + delec
 
-    return de
+    return de.get()
 
 
 class NAC(rhf_grad.GradientsBase):
@@ -289,7 +176,7 @@ class NAC(rhf_grad.GradientsBase):
         self.mol = td.mol
         self.base = td
         self.chkfile = td.chkfile
-        self.state = (1, 2)  # of which the gradients to be computed.
+        self.state = (0, 1)  # of which the gradients to be computed.
         self.atmlst = None
         self.de = None
 
