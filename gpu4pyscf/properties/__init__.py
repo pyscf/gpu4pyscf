57cf1d437adb820ce7f69f8872f2500c751bdd97
henryw7 <60555480+henryw7@users.noreply.github.com>
2025-05-30 07:17:37 -0700

    Raman intensities working, with numerical polarizability derivative (#420)
    * Raman intensities working, with numerical polarizability derivative
    
    * Add an example
    
    * Fix linter error
    
    * Forgot to change the example title
    
    * Update unittest.yml
    
    * Increase storage size
    
    * Update unittest.yml
    
    ---------
    
    Co-authored-by: Xiaojie Wu <wxj6000@gmail.com>

--- gpu4pyscf/properties/__init__.py
+++ gpu4pyscf/properties/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from gpu4pyscf.properties import polarizability, ir, shielding
+from gpu4pyscf.properties import polarizability, ir, shielding, raman
