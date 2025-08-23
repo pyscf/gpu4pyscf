A rough fix of the numerical stability. will be merging with changed master branch first

gpu4pyscf/pbc/dft/multigrid_v2.py
@@ -208,17 +208,45 @@ def screen_gaussian_pairs(
 
 
 def assign_pairs_to_blocks(
-    pairs_to_blocks_begin, pairs_to_blocks_end, n_blocks_abc, n_pairs, n_indices
+    pairs_to_blocks_begin,
+    pairs_to_blocks_end,
+    n_blocks_abc,
+    n_indices,
+    non_trivial_pairs,
+    i_shells,
+    j_shells,
+    image_indices,
+    vectors_to_neighboring_images,
+    mesh,
+    atm,
+    bas,
+    env
 ):
     n_blocks = np.prod(n_blocks_abc)
-    n_pairs_on_blocks = cp.full(n_blocks + 1, 0, dtype=cp.int32)
+    n_pairs_on_blocks = cp.zeros(n_blocks + 1, dtype=cp.int32)
+    n_unstable_pairs_on_blocks = cp.zeros(n_blocks + 1, dtype = cp.int32)
     err = libgpbc.count_pairs_on_blocks(
         cast_to_pointer(n_pairs_on_blocks),
+        cast_to_pointer(n_unstable_pairs_on_blocks),
         cast_to_pointer(pairs_to_blocks_begin),
         cast_to_pointer(pairs_to_blocks_end),
         cast_to_pointer(n_blocks_abc),
-        ctypes.c_int(n_pairs),
+        ctypes.c_int(len(non_trivial_pairs)),
+        cast_to_pointer(non_trivial_pairs),
+        cast_to_pointer(i_shells),
+        cast_to_pointer(j_shells),
+        ctypes.c_int(len(j_shells)),
+        cast_to_pointer(image_indices),
+        cast_to_pointer(vectors_to_neighboring_images),
+        ctypes.c_int(len(vectors_to_neighboring_images)),
+        cast_to_pointer(mesh),
+        cast_to_pointer(atm),
+        cast_to_pointer(bas),
+        cast_to_pointer(env)
     )
+    has_unstable_pairs = (n_unstable_pairs_on_blocks[-1] > 0)
+    print(n_unstable_pairs_on_blocks)
+    
     if err != 0:
         raise RuntimeError('count_pairs_on_blocks failed')
 
@@ -237,7 +265,18 @@ def assign_pairs_to_blocks(
         cast_to_pointer(pairs_to_blocks_end),
         cast_to_pointer(n_blocks_abc),
         ctypes.c_int(n_contributing_blocks),
-        ctypes.c_int(n_pairs),
+        ctypes.c_int(len(non_trivial_pairs)),
+        cast_to_pointer(non_trivial_pairs),
+        cast_to_pointer(i_shells),
+        cast_to_pointer(j_shells),
+        ctypes.c_int(len(j_shells)),
+        cast_to_pointer(image_indices),
+        cast_to_pointer(vectors_to_neighboring_images),
+        ctypes.c_int(len(vectors_to_neighboring_images)),
+        cast_to_pointer(mesh),
+        cast_to_pointer(atm),
+        cast_to_pointer(bas),
+        cast_to_pointer(env)
     )
 
     return (
@@ -293,6 +332,7 @@ def sort_gaussian_pairs(mydf, xc_type="LDA"):
         )
 
         dxyz_dabc = lattice_vectors / mesh[:,None]
+        libgpbc.update_dxyz_dabc(dxyz_dabc.ctypes)
         n_blocks_abc = np.asarray(np.ceil(mesh / block_size), dtype=cp.int32)
         equivalent_cell_in_localized, coeff_in_localized = (
             subcell_in_localized_region.decontract_basis(to_cart=True, aggregate=True)
@@ -402,7 +442,6 @@ def sort_gaussian_pairs(mydf, xc_type="LDA"):
                     contributing_block_ranges, axis=0
                 )
                 n_indices = int(cp.sum(n_contributing_blocks_per_pair))
-                n_pairs = len(screened_shell_pairs)
                 (
                     gaussian_pair_indices,
                     accumulated_counts,
@@ -411,8 +450,16 @@ def sort_gaussian_pairs(mydf, xc_type="LDA"):
                     pairs_to_blocks_begin,
                     pairs_to_blocks_end,
                     n_blocks_abc,
-                    n_pairs,
                     n_indices,
+                    screened_shell_pairs,
+                    i_shells,
+                    j_shells,
+                    image_indices,
+                    vectors_to_neighboring_images,
+                    mesh,
+                    atm,
+                    bas,
+                    env
                 )
                 t1 = log.timer_debug2(
                     "assigning pairs to blocks in angular pair"