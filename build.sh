0daeec5690f87e844d647b32f0cbe8e88e4638e9
Qiming Sun <qiming.sun@bytedance.com>
2025-10-12 12:47:31 -0700

    Fix build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ echo "CUDA_HOME=${CUDA_HOME}"
 export PATH="${CUDA_HOME}/bin:$PATH"
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
 
+mkdir build
 cmake -B build -S gpu4pyscf/lib -DCUDA_ARCHITECTURES=70 -DBUILD_LIBXC=OFF -DCMAKE_MESSAGE_LOG_LEVEL=WARNING
 cd build
 make -j8
