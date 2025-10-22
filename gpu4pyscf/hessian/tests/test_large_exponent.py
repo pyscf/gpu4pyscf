67226d26b88cf7ca4b3b83921536f75d3b9420b5
Henry Wang <henrywang0142857@gmail.com>
2025-10-22 15:41:43 -0700

    Turn on large_exponent rks tests, confirm the problem is from grid response

--- gpu4pyscf/hessian/tests/test_large_exponent.py
+++ gpu4pyscf/hessian/tests/test_large_exponent.py
@@ -19,7 +19,7 @@ import pyscf
 from gpu4pyscf import scf, dft
 
 def setUpModule():
-    global mol_minimal_one_atom, mol_minimal_two_atom, auxbasis_minimal_good, auxbasis_minimal_bad #, mol_real
+    global mol_minimal_one_atom, mol_minimal_two_atom, auxbasis_minimal_good, auxbasis_minimal_bad
     mol_minimal_one_atom = pyscf.M(
         atom = """
         He 100 200 300
@@ -52,27 +52,12 @@ def setUpModule():
             2.0 1.0
     """
 
-    # mol_real = pyscf.M(
-    #     atom = """
-    #         C      0.000000    0.000000    0.000000
-    #         Br     0.000000    0.000000    1.940000
-    #         H      1.027662    0.000000   -0.363333
-    #         H     -0.513831    0.889981   -0.363333
-    #         H     -0.513831   -0.889981   -0.363333
-    #     """,
-    #     basis = "def2-svp",
-    #     verbose = 4,
-    #     output='/dev/null',
-    # )
-
 def tearDownModule():
-    global mol_minimal_one_atom, mol_minimal_two_atom #, mol_real
+    global mol_minimal_one_atom, mol_minimal_two_atom
     mol_minimal_one_atom.stdout.close()
     del mol_minimal_one_atom
     mol_minimal_two_atom.stdout.close()
     del mol_minimal_two_atom
-    # mol_real.stdout.close()
-    # del mol_real
 
 class KnownValues(unittest.TestCase):
     def test_hessian_large_exp_one_atom_rhf(self):
@@ -170,64 +155,80 @@ class KnownValues(unittest.TestCase):
 
         assert np.max(np.abs(translation_invariance)) < 1e-4
 
-    # TODO: Fix DFT Hessian
+    def test_hessian_large_exp_one_atom_rks(self):
+        mf = dft.RKS(mol_minimal_one_atom, xc = "r2SCAN")
+        mf.grids.atom_grid = (50,194)
+        mf.conv_tol = 1e-10
 
-    # def test_hessian_large_exp_one_atom_rks(self):
-    #     mf = dft.RKS(mol_minimal_one_atom, xc = "LDA")
-    #     mf.grids.atom_grid = (200,194)
-    #     mf.conv_tol = 1e-10
+        mf.kernel()
+        assert mf.converged
 
-    #     mf.kernel()
-    #     assert mf.converged
+        hobj = mf.Hessian()
+        hobj.grid_response = True
+        hessian = hobj.kernel()
 
-    #     hobj = mf.Hessian()
-    #     hessian = hobj.kernel()
+        natm = mf.mol.natm
+        hessian = hessian.transpose(0,2,1,3)
+        hessian = hessian.reshape((natm * 3, natm * 3))
+        translation_invariance = np.sum(hessian, axis = 0).reshape(natm, 3)
 
-    #     natm = mf.mol.natm
-    #     hessian = hessian.transpose(0,2,1,3)
-    #     hessian = hessian.reshape((natm * 3, natm * 3))
-    #     translation_invariance = np.sum(hessian, axis = 0).reshape(natm, 3)
+        # Zero hessian for only one atom
+        assert np.max(np.abs(hessian)) < 1e-4
+        assert np.max(np.abs(translation_invariance)) < 1e-4
 
-    #     # Zero hessian for only one atom
-    #     assert np.max(np.abs(hessian)) < 1e-3
-    #     assert np.max(np.abs(translation_invariance)) < 1e-3
+    def test_hessian_large_exp_two_atom_rks(self):
+        mf = dft.RKS(mol_minimal_two_atom, xc = "wB97X")
+        mf.grids.atom_grid = (50,194)
+        mf.conv_tol = 1e-10
 
-    # def test_hessian_large_exp_two_atom_rks(self):
-    #     mf = dft.RKS(mol_minimal_two_atom, xc = "LDA")
-    #     mf.grids.atom_grid = (200,194)
-    #     mf.conv_tol = 1e-10
+        mf.kernel()
+        assert mf.converged
 
-    #     mf.kernel()
-    #     assert mf.converged
+        hobj = mf.Hessian()
+        hobj.grid_response = True
+        hessian = hobj.kernel()
 
-    #     hobj = mf.Hessian()
-    #     hessian = hobj.kernel()
+        natm = mf.mol.natm
+        hessian = hessian.transpose(0,2,1,3)
+        hessian = hessian.reshape((natm * 3, natm * 3))
+        translation_invariance = np.sum(hessian, axis = 0).reshape(natm, 3)
 
-    #     natm = mf.mol.natm
-    #     hessian = hessian.transpose(0,2,1,3)
-    #     hessian = hessian.reshape((natm * 3, natm * 3))
-    #     translation_invariance = np.sum(hessian, axis = 0).reshape(natm, 3)
+        assert np.max(np.abs(translation_invariance)) < 1e-4
 
-    #     assert np.max(np.abs(translation_invariance)) < 1e-3
+    @pytest.mark.skip("Too slow, functionality covered by corresponding tests above")
+    def test_hessian_large_exp_methylbromide_rks(self):
+        mol_real = pyscf.M(
+            atom = """
+                C      0.000000    0.000000    0.000000
+                Br     0.000000    0.000000    1.940000
+                H      1.027662    0.000000   -0.363333
+                H     -0.513831    0.889981   -0.363333
+                H     -0.513831   -0.889981   -0.363333
+            """,
+            basis = "def2-svp",
+            verbose = 4,
+            output='/dev/null',
+        )
 
-    # def test_hessian_large_exp_methylbromide_rks(self):
-    #     mf = dft.RKS(mol_real, xc = "wB97X")
-    #     mf.grids.atom_grid = (99,590)
-    #     mf = mf.density_fit(auxbasis = "def2-universal-jkfit")
-    #     mf.conv_tol = 1e-10
+        mf = dft.RKS(mol_real, xc = "wB97X")
+        mf.grids.atom_grid = (99,590)
+        mf = mf.density_fit(auxbasis = "def2-universal-jkfit")
+        mf.conv_tol = 1e-10
 
-    #     mf.kernel()
-    #     assert mf.converged
+        mf.kernel()
+        assert mf.converged
 
-    #     hobj = mf.Hessian()
-    #     hessian = hobj.kernel()
+        hobj = mf.Hessian()
+        hobj.auxbasis_response = 2
+        hobj.grid_response = True
+        hessian = hobj.kernel()
 
-    #     natm = mf.mol.natm
-    #     hessian = hessian.transpose(0,2,1,3)
-    #     hessian = hessian.reshape((natm * 3, natm * 3))
-    #     translation_invariance = np.sum(hessian, axis = 0).reshape(natm, 3)
+        natm = mf.mol.natm
+        hessian = hessian.transpose(0,2,1,3)
+        hessian = hessian.reshape((natm * 3, natm * 3))
+        translation_invariance = np.sum(hessian, axis = 0).reshape(natm, 3)
 
-    #     assert np.max(np.abs(translation_invariance)) < 1e-10
+        assert np.max(np.abs(translation_invariance)) < 1e-7
 
 if __name__ == "__main__":
     print("Edge Case Tests for Hessian Calculation with Large Exponent in Atomic Orbitals")
