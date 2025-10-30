d870f8a95ae47817b3e8ea8ab57c6f28986a8aee
puzhichen.996 <puzhichen.996@bytedance.com>
2025-10-30 16:08:58 +0800

    begin to write

---
+++ gpu4pyscf/dft/tests/test_gks.py
@@ -0,0 +1,28 @@
+# Copyright 2021-2024 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pickle
+import numpy as np
+import unittest
+import pyscf
+from gpu4pyscf.dft import rks
+
+atom = '''
+O       0.0000000000    -0.0000000000     0.1174000000
+H      -0.7570000000    -0.0000000000    -0.4696000000
+H       0.7570000000     0.0000000000    -0.4696000000
+'''
+bas='def2-tzvpp'
+grids_level = 5
+nlcgrids_level = 2
