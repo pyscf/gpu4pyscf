38925a542bdc474c4a815e35068530b1c57ed3f0
henryw7 <60555480+henryw7@users.noreply.github.com>
2025-11-19 22:25:54 -0800

    Minor bug fix (#567)
    * Minor bug fix
    
    * Make the linter happy

--- gpu4pyscf/_patch_pyscf.py
+++ gpu4pyscf/_patch_pyscf.py
@@ -15,6 +15,7 @@
 import numpy as np
 import cupy
 import pyscf
+from pyscf import lib
 
 pyscf_version = int(pyscf.__version__.split('.')[1])
 
@@ -38,7 +39,6 @@ if pyscf_version <= 10:
     GDF.to_gpu = _gdf_to_gpu
 
     # patch PySCF Cell class, updating lattice parameters is not avail in pyscf 2.10
-    from pyscf import lib
     from pyscf.lib import logger
     from pyscf.gto import mole
     from pyscf.pbc.gto.cell import Cell
