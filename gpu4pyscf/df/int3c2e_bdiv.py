Use q_cond cuda kernel in rsjk initialization; Fix overlap strain derivatives

gpu4pyscf/df/int3c2e_bdiv.py
@@ -313,7 +313,8 @@ class Int3c2eOpt:
 
         # nst_lookup stores the nst_per_block for each (li,lj,lk) pattern
         omega = self.sorted_mol.omega
-        nst_lookup = asarray(create_nst_lookup_table(omega), dtype=np.int32)
+        nst_lookup, max_shm_size = asarray(create_nst_lookup_table(omega), dtype=np.int32)
+        max_shm_size = max_shm_size.max()
 
         shl_pair_idx = asarray(np.hstack(self.shl_pair_idx), dtype=np.int32)
         shl_pair_offsets = asarray(self.shl_pair_offsets, dtype=np.int32)
@@ -344,7 +345,7 @@ class Int3c2eOpt:
             err = kern(
                 ctypes.cast(eri3c.data.ptr, ctypes.c_void_p),
                 ctypes.byref(int3c2e_envs),
-                ctypes.c_int(SHM_SIZE), ctypes.c_int(naux_batch),
+                ctypes.c_int(max_shm_size), ctypes.c_int(naux_batch),
                 ctypes.c_int(nbatches_shl_pair), ctypes.c_int(nblocks),
                 ctypes.c_int(ksh_offsets[start]),
                 ctypes.cast(shl_pair_offsets.data.ptr, ctypes.c_void_p),
@@ -681,15 +682,36 @@ def int3c2e_scheme(li, lj, lk, omega=0, shm_size=SHM_SIZE):
     return nst_per_block, gout_stride
 
 def create_nst_lookup_table(omega=0):
-    nst_lookup = np.zeros([L_AUX_MAX+1]*3, dtype=np.int32)
-    for lk in range(L_AUX_MAX+1):
-        for li in range(lk+1):
-            for lj in range(li+1):
-                nst_lookup[lk,li,lj] = int3c2e_scheme(li, lj, lk, omega)[0]
-    idx = np.arange(L_AUX_MAX+1)
-    z, y, x = np.sort(np.meshgrid(idx, idx, idx), axis=0)
-    nst_lookup = nst_lookup[x, y, z]
-    return nst_lookup[:,:LMAX+1,:LMAX+1]
+    ls = np.arange(LMAX+1)
+    li = ls[:,None]
+    lj = ls
+    laux = np.arange(L_AUX_MAX+1)[:,None,None]
+    order = laux + li + lj
+    nroots = order // 2 + 1
+    if omega < 0:
+        nroots *= 2
+    g_size = (laux+1)*(li+1)*(lj+1)
+    unit = g_size*3 + nroots*2 + 7
+    shm_size = SHM_SIZE - 1024
+    nst_max = shm_size // (unit*8)
+    # Round to the nearest power2
+    nst_max = _nearest_power2(nst_max)
+
+    nfi = (li + 1) * (li + 2) // 2
+    nfj = (lj + 1) * (lj + 2) // 2
+    nfaux = (laux + 1) * (laux + 2) // 2
+    gout_size = nfaux * nfi * nfj
+    gout_stride = (gout_size + GOUT_WIDTH-1) // GOUT_WIDTH
+    # Round up to the next 2^n
+    gout_stride = _nearest_power2(gout_stride, return_leq=False)
+    #gout_stride[gout_stride>=64] = 64
+
+    nst_per_block = THREADS // gout_stride
+    # min(nst_per_block, nst_max)
+    nst_per_block = np.where(nst_per_block < nst_max, nst_per_block, nst_max)
+    shm_size = nst_per_block * (unit * 8)
+    #shm_size += (nfaux + nfi + nfj) * (3 * 4)
+    return nst_per_block, shm_size
 
 def estimate_shl_ovlp(mol):
     # consider only the most diffuse component of a basis

gpu4pyscf/pbc/gto/tests/test_pbc_int1e.py
@@ -141,11 +141,11 @@ def test_int1e_ovlp1():
     assert abs(k.get() - ref).max() < 1e-10
 
 def test_ovlp_stress_tensor():
-    a = np.eye(3) * 6.
+    a = np.eye(3) * 5.
     np.random.seed(5)
     a += np.random.rand(3, 3) - .5
     cell = pyscf.M(
-        atom='He 1 1 1; He 2 1.5 2.4',
+        atom='He 1 1 1; He 2 1.5 2.4; He 3 3.5 0.2',
         basis=[[0, [.5, 1]],
                [1, [1.5, 1], [.5, 1]],
                [2, [.8, 1]],

gpu4pyscf/scf/jk.py
@@ -271,10 +271,10 @@ def apply_coeff_CT_mat_C(cartesian_matrix, mol, sorted_mol, uniq_l_ctr,
 
 
 class _VHFOpt:
-    def __init__(self, mol, cutoff=1e-13, tile=TILE):
+    def __init__(self, mol, direct_scf_tol=1e-13, tile=TILE):
         self.mol = mol
         self.sorted_mol = None
-        self.direct_scf_tol = cutoff
+        self.direct_scf_tol = direct_scf_tol
         self.uniq_l_ctr = None
         self.l_ctr_offsets = None
         self.h_shls = None
@@ -1121,6 +1121,13 @@ def _nearest_power2(n, return_leq=True):
         return_leq specifies that the return is less or equal than n.
         Otherwise, the return is greater or equal than n.
     '''
+    if isinstance(n, np.ndarray):
+        n = n.astype(int, copy=False)
+        if return_leq:
+            return 2 ** np.log2(n).astype(int)
+        else:
+            return 2 ** np.ceil(np.log2(n)).astype(int)
+
     n = int(n)
     assert n > 0
     if return_leq:
@@ -1129,50 +1136,44 @@ def _nearest_power2(n, return_leq=True):
         return 1 << ((n-1).bit_length())
 
 def _create_q_cond(mol, uniq_l_ctr, l_ctr_offsets, envs, precision=1e-14):
-    gout_stride_lookup = np.empty([LMAX+1,LMAX+1], dtype=np.int32)
+    '''A fast routine to estimate the Schwarz inequality condition sqrt(absmax( (ij|ij) )).
+    Note the high angular momentum bases are excluded.
+    '''
+    from gpu4pyscf.pbc.gto import int1e
     gout_width = 60
-    shm_size = SHM_SIZE
-    uniq_l = uniq_l_ctr[:,0]
-    lmax = uniq_l.max()
-    ls = np.arange(lmax+1)
-    nf = (ls+1) * (ls+2) // 2
-    max_shm_size = 0
     omega = mol.omega
-    for li in range(lmax+1):
-        for lj in range(li+1):
-            lij = li + lj
-            nroots = lij + 1
-            if omega < 0:
-                nroots *= 2
-            unit = (li+1)*(lj+1)*2 + (li+1)*(lj+1)*(lij+1) + 6 + nroots*4
-            nsp_max = _nearest_power2(shm_size // (unit*4))
-            gout_size = nf[li] * nf[lj]
-            gout_stride = (gout_size+gout_width-1) / gout_width
-            gout_stride = _nearest_power2(gout_stride, return_leq=False)
-            nsp_per_block = min(nsp_max, THREADS // gout_stride)
-            gout_stride = THREADS // nsp_per_block
-            gout_stride_lookup[li,lj] = gout_stride_lookup[lj,li] = gout_stride
-            max_shm_size = max(max_shm_size, nsp_per_block*unit*4)
+    ls = np.arange(LMAX+1)
+    li = ls[:,None]
+    lj = ls
+    lij = li + lj
+    nfi = (li + 1) * (li + 2) // 2
+    nfj = (lj + 1) * (lj + 2) // 2
+    nroots = lij + 1
+    if omega < 0:
+        nroots *= 2
+    unit = (li+1)*(lj+1)*2 + (li+1)*(lj+1)*(lij+1) + 6 + nroots*4
+    nsp_max = _nearest_power2(SHM_SIZE // (unit*4))
+    gout_size = nfi * nfj
+    gout_stride = (gout_size+gout_width-1) // gout_width
+    gout_stride = _nearest_power2(gout_stride, return_leq=False)
+    nsp_per_block = THREADS // gout_stride
+    # min(nsp_per_block, nsp_max)
+    nsp_per_block = np.where(nsp_per_block < nsp_max, nsp_per_block, nsp_max)
+    gout_stride = THREADS // nsp_per_block
+    shm_size = nsp_per_block * (unit * 4)
+    max_shm_size = shm_size.max()
 
-    # important shell-pairs based on distance
-    nbas = mol.nbas
-    ovlp_mask = cp.zeros((nbas,nbas), dtype=bool)
-    exps, cs = extract_pgto_params(mol, 'diffuse')
-    exps = cp.asarray(exps, dtype=np.float32)
-    theta_ij = exps[:,None] * exps
-    theta_ij /= exps[:,None] + exps
-    bas_coords = cp.asarray(mol.atom_coords()[mol._bas[:,gto.ATOM_OF]])
-    rr = cp.asarray(dist_matrix(bas_coords, bas_coords), dtype=np.float32)
-    rr **= 2
-    theta_rr = theta_ij * rr
-    norm_penalty = 10
-    ovlp_mask = theta_rr < norm_penalty - 2*math.log(precision)
-
+    ovlp_mask = int1e._shell_overlap_mask(mol, precision=precision**2)
+    nbas = np.uint32(mol.nbas)
+    assert nbas < 65535
+    uniq_l = uniq_l_ctr[:,0]
     bas_ij_idx = [] # The effective shell pair = ish*nbas+jsh
     shl_pair_offsets = [] # the bas_ij_idx offset for each blockIdx.x
     sp0 = sp1 = 0
     for i, li in enumerate(uniq_l):
         for j, lj in enumerate(uniq_l[:i+1]):
+            if li > LMAX or lj > LMAX:
+                continue
             ish0, ish1 = l_ctr_offsets[i], l_ctr_offsets[i+1]
             jsh0, jsh1 = l_ctr_offsets[j], l_ctr_offsets[j+1]
             ish = cp.arange(ish0, ish1, dtype=np.uint32)
@@ -1182,7 +1183,7 @@ def _create_q_cond(mol, uniq_l_ctr, l_ctr_offsets, envs, precision=1e-14):
             nshl_pair = len(idx)
             bas_ij_idx.append(idx)
             sp0, sp1 = sp1, sp1 + nshl_pair
-            nsp_per_block = THREADS // gout_stride_lookup[li, lj] * 8
+            nsp_per_block = THREADS // gout_stride[li, lj] * 8
             shl_pair_offsets.append(np.arange(sp0, sp1, nsp_per_block, dtype=np.int32))
     ovlp_mask = None
     shl_pair_offsets.append(np.int32(sp1))
@@ -1207,7 +1208,7 @@ def _create_q_cond(mol, uniq_l_ctr, l_ctr_offsets, envs, precision=1e-14):
         lr_factor = 0
     if omega > 0:
         sr_factor = 0
-    gout_stride_lookup = cp.asarray(gout_stride_lookup, dtype=np.int32)
+    gout_stride = cp.asarray(gout_stride, dtype=np.int32)
     libvhf_rys.int2e_qcond_estimator(
         ctypes.cast(q_out.data.ptr, ctypes.c_void_p),
         s_out_ptr,
@@ -1216,7 +1217,7 @@ def _create_q_cond(mol, uniq_l_ctr, l_ctr_offsets, envs, precision=1e-14):
         ctypes.c_int(nbatches_shl_pair),
         ctypes.cast(bas_ij_idx.data.ptr, ctypes.c_void_p),
         ctypes.cast(shl_pair_offsets.data.ptr, ctypes.c_void_p),
-        ctypes.cast(gout_stride_lookup.data.ptr, ctypes.c_void_p),
+        ctypes.cast(gout_stride.data.ptr, ctypes.c_void_p),
         ctypes.c_double(omega),
         ctypes.c_double(lr_factor),
         ctypes.c_double(sr_factor))

gpu4pyscf/scf/tests/test_scf_jk.py
@@ -320,43 +320,38 @@ def test_q_cond():
     mol = pyscf.M(
         atom = '''
         O   0.000   -0.    0.1174
-        H  -0.757    4.   -0.4696
-        H   0.757    4.   -0.4696
-        C   1.      1.    0.
         H   4.      0.    3.
         H   0.      1.    .6
-        O   -3.2258  -0.1262  2.6126
+        C   -3.2258  -0.1262  2.6126
         H   -5.7987   0.2177  4.1423
         H   -5.8042  -1.0067  4.1503
         ''',
-        basis='def2-tzvp'
+        basis=('def2-tzvp', [[4, [1, 1]]]),
     )
 
     jkopt = jk._VHFOpt(mol).build()
     sorted_mol = group_basis(mol)[0]
     qref, sref = q_cond_reference(sorted_mol)
     q_cond = jkopt.q_cond.get()
-    qref[qref < -30] = -30
-    q_cond[q_cond < -30] = -30
-    assert abs(qref - q_cond).max() < 5e-5
+    thrd = np.log(jkopt.direct_scf_tol)
+    qref[qref < thrd] = thrd
+    q_cond[q_cond < thrd] = thrd
+    assert abs(qref - q_cond).max() < 1e-3
 
     mol.omega = .25
     jkopt = jk._VHFOpt(mol).build()
     sorted_mol = group_basis(mol)[0]
     qref, sref = q_cond_reference(sorted_mol)
     q_cond = jkopt.q_cond.get()
-    qref[qref < -30] = -30
-    q_cond[q_cond < -30] = -30
-    assert abs(qref - q_cond).max() < 5e-5
+    qref[qref < thrd] = thrd
+    q_cond[q_cond < thrd] = thrd
+    assert abs(qref - q_cond).max() < 1e-3
 
-    mol.omega = -.3
+    mol.omega = -.25
     jkopt = jk._VHFOpt(mol).build()
     sorted_mol = group_basis(mol)[0]
     qref, sref = q_cond_reference(sorted_mol)
     q_cond = jkopt.q_cond.get()
-    qref[qref < -30] = -30
-    q_cond[q_cond < -30] = -30
-    assert abs(qref - q_cond).max() < 5e-5
-    #if mol.omega < 0:
-    #    s_estimator = jkopt.s_estimator[:mol.nbas].get()
-    #    print((s_estimator - sref[:mol.nbas]).min())
+    qref[qref < thrd] = thrd
+    q_cond[q_cond < thrd] = thrd
+    assert abs(qref - q_cond).max() < 1e-3