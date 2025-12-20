Fix int32 overflow error in examples/40-all_electron_scf.py (#605)
* Fix int32 overflow error in examples/40-all_electron_scf.py

* More overflows

* More int32 overflow problem in C script

* Remove unnecessay code, cupy.cuda.PinnedMemory() is actually able to handle int64

gpu4pyscf/lib/cupy_helper.py
@@ -27,6 +27,7 @@ from gpu4pyscf.lib.memcpy import copy_array, p2p_transfer  #NOQA
 from gpu4pyscf.lib import multi_gpu
 from gpu4pyscf.lib.utils import load_library
 from gpu4pyscf.__config__ import num_devices, _p2p_access
+import cupyx
 
 LMAX_ON_GPU = 7
 DSOLVE_LINDEP = 1e-13
@@ -803,7 +804,9 @@ def empty_mapped(shape, dtype=float, order='C'):
     except that the underlying buffer is a pinned and mapped memory.
     This array can be used as the buffer of zero-copy memory.
     '''
-    nbytes = np.prod(shape) * np.dtype(dtype).itemsize
+    size = int(np.prod(shape))
+    nbytes = size * int(np.dtype(dtype).itemsize)
+    assert nbytes >= 0, f"nbytes = {nbytes} is negative, type(nbytes) = {type(nbytes)}, please check if overflow happens"
     mem = cupy.cuda.PinnedMemoryPointer(
         cupy.cuda.PinnedMemory(nbytes, cupy.cuda.runtime.hostAllocMapped), 0)
     out = np.ndarray(shape, dtype=dtype, buffer=mem, order=order)