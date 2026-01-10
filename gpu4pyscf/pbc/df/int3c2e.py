Fix df krhf gradients

gpu4pyscf/pbc/df/int3c2e.py
@@ -159,75 +159,32 @@ def sr_int2c2e(auxcell, omega, kpts=None, bvk_kmesh=None):
     '''SR 2c2e Coulomb integrals for the auxiliary basis set'''
     from gpu4pyscf.df.int3c2e_bdiv import int2c2e_scheme
     assert omega < 0
-    cell = SortedCell.from_cell(auxcell)
-    lmax = cell.uniq_l_ctr[:,0].max()
-    assert lmax <= L_AUX_MAX
-
-    nsp_per_block, gout_stride, shm_size = int2c2e_scheme(omega, gout_width=60)
-    gout_stride = cp.asarray(gout_stride, dtype=np.int32)
-    shm_size_max = shm_size[:lmax+1,:lmax+1].max()
-
+    cell = auxcell
     if bvk_kmesh is None:
         bvk_kmesh = kpts_to_kmesh(cell, kpts, bound_by_supmol=True)
-    bvk_ncells = np.prod(bvk_kmesh)
-    if bvk_ncells == 1:
-        bvkcell = cell
-    else:
-        bvkcell = pbctools.super_cell(cell, bvk_kmesh, wrap_around=True)
-        # PTR_BAS_COORD was not initialized in pbctools.supe_rcell
-        bvkcell._bas[:,PTR_BAS_COORD] = bvkcell._atm[bvkcell._bas[:,ATOM_OF],PTR_COORD]
+    int2c2e_opt = SRInt2c2eOpt(cell, omega, bvk_kmesh).build()
+    cell = int2c2e_opt.cell
+    bvk_ncells = len(int2c2e_opt.bvkmesh_Ls)
 
-    rcut = _estimate_sr_2c2e_rcut(cell.cell, omega, cell.precision*1e-3)
-    Ls = asarray(bvkcell.get_lattice_Ls(rcut=rcut))
-    Ls = Ls[cp.linalg.norm(Ls-.5, axis=1).argsort()]
-    nimgs = len(Ls)
+    nsp_per_block, gout_stride, shm_size = int2c2e_scheme(omega, gout_width=60)
+    lmax = cell.uniq_l_ctr[:,0].max()
+    shm_size_max = shm_size[:lmax+1,:lmax+1].max()
+    gout_stride = cp.asarray(gout_stride, dtype=np.int32)
 
-    bas_ij_idx = [] # The effective shell pair = ish*nbas+jsh
-    shl_pair_offsets = [] # the bas_ij_idx offset for each blockIdx.x
-    sp0 = sp1 = 0
-    nbas = cell.nbas
-    l_ctr_offsets = np.append(0, np.cumsum(cell.l_ctr_counts))
-    uniq_l = cell.uniq_l_ctr[:,0]
-    ij_tasks = [(i, j) for i in range(len(uniq_l)) for j in range(i+1)]
-    for i, j in ij_tasks:
-        li = uniq_l[i]
-        lj = uniq_l[j]
-        ish0, ish1 = l_ctr_offsets[i], l_ctr_offsets[i+1]
-        jsh0, jsh1 = l_ctr_offsets[j], l_ctr_offsets[j+1]
-        ish = cp.arange(ish0, ish1, dtype=np.int32)
-        jsh = cp.arange(jsh0, jsh1, dtype=np.int32)
-        img = cp.arange(bvk_ncells, dtype=np.int32)
-        ijsh = ish[:,None] * (nbas*bvk_ncells) + jsh
-        if i == j:
-            ijsh = ijsh[cp.tril_indices(ish1-ish0)]
-        else:
-            ijsh = ijsh.ravel()
-        idx = (img[:,None] * nbas + ijsh).ravel()
-        nshl_pair = len(idx)
-        bas_ij_idx.append(idx)
-        sp0, sp1 = sp1, sp1 + nshl_pair
-        batch_size = nsp_per_block[li, lj] * 4
-        shl_pair_offsets.append(np.arange(sp0, sp1, batch_size, dtype=np.int32))
-    shl_pair_offsets.append(np.array([sp1], dtype=np.int32))
-    shl_pair_offsets = cp.array(np.hstack(shl_pair_offsets), dtype=np.int32)
-    bas_ij_idx = cp.array(cp.hstack(bas_ij_idx), dtype=np.int32)
+    bas_ij_idx, shl_pair_offsets = cell.aggregate_shl_pairs(
+        int2c2e_opt.bas_ij_cache, nsp_per_block*4)
 
-    with bvkcell.with_range_coulomb(omega):
-        ao_loc = bvkcell.ao_loc
-        _env = _scale_sp_ctr_coeff(bvkcell)
-        rys_envs = PBCIntEnvVars.new(
-            cell.natm, cell.nbas, bvk_ncells, nimgs,
-            bvkcell._atm, bvkcell._bas, _env, ao_loc, Ls)
-        nbatches_shl_pair = len(shl_pair_offsets) - 1
-        nao = cell.nao
-        out = cp.empty((bvk_ncells, nao, nao))
-        err = libpbc.fill_int2c2e(
-            ctypes.cast(out.data.ptr, ctypes.c_void_p),
-            ctypes.byref(rys_envs), ctypes.c_int(shm_size_max),
-            ctypes.c_int(nbatches_shl_pair),
-            ctypes.cast(shl_pair_offsets.data.ptr, ctypes.c_void_p),
-            ctypes.cast(bas_ij_idx.data.ptr, ctypes.c_void_p),
-            ctypes.cast(gout_stride.data.ptr, ctypes.c_void_p))
+    nbatches_shl_pair = len(shl_pair_offsets) - 1
+    rys_envs = int2c2e_opt._rys_envs
+    nao = cell.nao
+    out = cp.empty((bvk_ncells, nao, nao))
+    err = libpbc.fill_int2c2e(
+        ctypes.cast(out.data.ptr, ctypes.c_void_p),
+        ctypes.byref(rys_envs), ctypes.c_int(shm_size_max),
+        ctypes.c_int(nbatches_shl_pair),
+        ctypes.cast(shl_pair_offsets.data.ptr, ctypes.c_void_p),
+        ctypes.cast(bas_ij_idx.data.ptr, ctypes.c_void_p),
+        ctypes.cast(gout_stride.data.ptr, ctypes.c_void_p))
     if err != 0:
         raise RuntimeError('fill_int2c2e failed')
     out = fill_triu_bvk_conj(out, nao, bvk_kmesh)
@@ -282,6 +239,7 @@ class SRInt3c2eOpt:
         assert self.cell.uniq_l_ctr[:,0].max() <= LMAX
         self.auxcell = SortedCell.from_cell(
             auxcell, allow_replica=True, allow_split_seg_contraction=False)
+        assert self.auxcell.uniq_l_ctr[:,0].max() <= L_AUX_MAX
         self.cell.omega = omega
         self.auxcell.omega = omega
         # Adjust the rcut because the default cell.rcut is estimated based on
@@ -679,6 +637,74 @@ class SRInt3c2eOpt:
         vj = transpose_sum(vj)
         vj = cell.apply_CT_mat_C(vj)
         return vj
+
+class SRInt2c2eOpt:
+    def __init__(self, cell, omega, bvk_kmesh=None):
+        assert omega < 0
+        self.omega = -omega
+        self.cell = SortedCell.from_cell(
+            cell, allow_replica=True, allow_split_seg_contraction=False)
+        assert self.cell.uniq_l_ctr[:,0].max() <= LMAX
+        self.cell.omega = omega
+
+        if bvk_kmesh is None:
+            bvk_kmesh = np.ones(3, dtype=int)
+        self.bvk_kmesh = bvk_kmesh
+
+        self.rcut = None
+        self._rys_envs = None
+        self.bas_ij_cache = None
+        self.bvkcell = None
+        self.bvkmesh_Ls = None
+
+    def build(self):
+        cell = self.cell
+        omega = self.omega
+        bvk_kmesh = self.bvk_kmesh
+        bvk_ncells = np.prod(bvk_kmesh)
+        self.bvkmesh_Ls = translation_vectors_for_kmesh(cell, bvk_kmesh, True)
+        if np.prod(bvk_kmesh) == 1:
+            bvkcell = cell
+        else:
+            bvkcell = pbctools.super_cell(cell, bvk_kmesh, wrap_around=True)
+            # PTR_BAS_COORD was not initialized in pbctools.supe_rcell
+            bvkcell._bas[:,PTR_BAS_COORD] = bvkcell._atm[bvkcell._bas[:,ATOM_OF],PTR_COORD]
+        bvkcell.omega = -omega
+        self.bvkcell = bvkcell
+
+        if self.rcut is None:
+            self.rcut = _estimate_sr_2c2e_rcut(cell, omega, cell.precision*1e-3)
+        Ls = asarray(bvkcell.get_lattice_Ls(rcut=self.rcut))
+        Ls = Ls[cp.linalg.norm(Ls-.5, axis=1).argsort()]
+        nimgs = len(Ls)
+        logger.debug(cell, 'int2c2e_kernel rcut = %g, nimgs = %d', self.rcut, nimgs)
+
+        _env = _scale_sp_ctr_coeff(bvkcell)
+        self._rys_envs = PBCIntEnvVars.new(
+            cell.natm, cell.nbas, bvk_ncells, nimgs,
+            bvkcell._atm, bvkcell._bas, _env, bvkcell.ao_loc, Ls)
+
+        self.bas_ij_cache = bas_ij_cache = {}
+        nbas = cell.nbas
+        l_ctr_offsets = np.append(0, np.cumsum(cell.l_ctr_counts))
+        uniq_l = cell.uniq_l_ctr[:,0]
+        ij_tasks = [(i, j) for i in range(len(uniq_l)) for j in range(i+1)]
+        for i, j in ij_tasks:
+            li = uniq_l[i]
+            lj = uniq_l[j]
+            ish0, ish1 = l_ctr_offsets[i], l_ctr_offsets[i+1]
+            jsh0, jsh1 = l_ctr_offsets[j], l_ctr_offsets[j+1]
+            ish = cp.arange(ish0, ish1, dtype=np.int32)
+            jsh = cp.arange(jsh0, jsh1, dtype=np.int32)
+            img = cp.arange(bvk_ncells, dtype=np.int32)
+            ijsh = ish[:,None] * (nbas*bvk_ncells) + jsh
+            if i == j:
+                ijsh = ijsh[cp.tril_indices(ish1-ish0)]
+            else:
+                ijsh = ijsh.ravel()
+            idx = (img[:,None] * nbas + ijsh).ravel()
+            bas_ij_cache[i, j] = cp.asarray(idx, dtype=np.uint32)
+        return self
 
 def _conc_locs(ao_loc1, ao_loc2):
     comp_loc = np.append(ao_loc1[:-1], ao_loc1[-1] + ao_loc2)

gpu4pyscf/pbc/df/rsdf_builder.py
@@ -691,12 +691,25 @@ def _unpack_cderi_v2(cderi_compressed, pair_address, kj_idx, conj_mapping,
     if is_gamma_point:
         return cderi.transpose(1,3,0,2)
 
+    assert nkpts == len(conj_mapping)
+    assert nkpts == len(kj_idx)
     assert expLk.dtype == np.complex128
     if axis == 0:
         # j is reordered so that the corresponding index i is sorted
         expLk_j = expLk[:,kj_idx]
+        # index j in out has been transformed to an order corresponding to index
+        # i in [0...Nk] order. The original kpt for each transformed j-index is
+        # provided by the kj_idx.
+        conj_ki_order = conj_mapping[kj_idx]
     else:
         expLk_j = expLk
+        conj_ki_order = np.empty(nkpts, dtype=np.int32)
+        # index j in out has been transformed to the order [0...Nk]
+        # The associated index i must be reordered to the argsort(kj_idx)
+        # The conj_mapping corresponds to conj(expLk) for transforming index i
+        conj_ki_order[kj_idx] = conj_mapping # conj_mapping[ki_idx]
+    conj_ki_order = cp.asarray(conj_ki_order, dtype=np.int32)
+
     if cderi.dtype == np.complex128:
         out = contract('iLjk,LK->Kijk', cderi, expLk_j, out=out)
     else:
@@ -704,8 +717,6 @@ def _unpack_cderi_v2(cderi_compressed, pair_address, kj_idx, conj_mapping,
         out = contract('iLjk,LKz->Kijkz', cderi, expLkz, out=out)
         out = out.view(np.complex128)[:,:,:,:,0]
 
-    assert nkpts == len(conj_mapping)
-    assert nkpts == len(kj_idx)
     # tril_idx in the reference cell associated to the pair_address.
     # Note indices within this array does not guarantee i>=j. It only indicates
     # the unique pairs for each unit cell.
@@ -713,19 +724,6 @@ def _unpack_cderi_v2(cderi_compressed, pair_address, kj_idx, conj_mapping,
     mask[pair_address] = True
     mask = cp.any(mask.reshape(nao, nL, nao), axis=1)
     tril_idx = cp.asarray(cp.where(mask.ravel())[0], dtype=np.int32)
-
-    if axis == 0:
-        # index j in out has been transformed to an order corresponding to index
-        # i in [0...Nk] order. The original kpt for each transformed j-index is
-        # provided by the kj_idx.
-        conj_ki_order = conj_mapping[kj_idx]
-    else:
-        conj_ki_order = np.empty(nkpts, dtype=np.int32)
-        # index j in out has been transformed to the order [0...Nk]
-        # The associated index i must be reordered to the argsort(kj_idx)
-        # The conj_mapping corresponds to conj(expLk) for transforming index i
-        conj_ki_order[kj_idx] = conj_mapping # conj_mapping[ki_idx]
-    conj_ki_order = cp.asarray(conj_ki_order, dtype=np.int32)
 
     libpbc.fill_indexed_triu(
         ctypes.cast(out.data.ptr, ctypes.c_void_p),

gpu4pyscf/pbc/df/tests/test_pbc_df_grad.py
@@ -17,9 +17,11 @@ import numpy as np
 import cupy as cp
 import pyscf
 from pyscf import lib
+from gpu4pyscf.lib.cupy_helper import tag_array
 from gpu4pyscf.pbc.df import int3c2e
 from gpu4pyscf.pbc.df.grad import rhf
 from gpu4pyscf.pbc.df.grad import krhf
+from gpu4pyscf.gto.mole import SortedGTO
 
 def test_ej_ip1_gamma_point():
     cell = pyscf.M(
@@ -50,7 +52,7 @@ C    P
       0.4000000000           1.0000000000
 C    D
       0.1995412500           1.0000000000 ''',
-        'C2':[[0, [.5, 1.]]],
+        'C2': ('unc-weigend', [[0, [.5, 1.]], [1, [.8, 1.]], [3, [.9, 1]]]),
     }
     auxcell.build()
     omega = -0.2
@@ -61,18 +63,20 @@ C    D
     mo_coeff = np.random.rand(nao, nao) - .5
     mo_occ = np.zeros(nao)
     mo_occ[:nocc] = 2
-    opt = int3c2e.SRInt3c2eOpt_v2(cell, auxcell, omega).build()
-    ej = rhf._jk_energy_per_atom(opt, mo_coeff, mo_occ, k_factor=0)
-    assert abs(ej.sum(axis=0)).max() < 1e-12
-
     dm = (mo_coeff*mo_occ).dot(mo_coeff.T)
+    dm = tag_array(dm, mo_coeff=mo_coeff, mo_occ=mo_occ)
+    opt = int3c2e.SRInt3c2eOpt(cell, auxcell, omega).build()
+    ej = rhf._jk_energy_per_atom(opt, dm, hermi=1, k_factor=0)
+    assert abs(ej.sum(axis=0)).max() < 1e-11
+
     disp = 1e-3
     atom_coords = cell.atom_coords()
+    dm = SortedGTO.from_cell(cell).apply_C_mat_CT(dm)
     def eval_j(i, x, disp):
         atom_coords[i,x] += disp
         cell1 = cell.set_geom_(atom_coords, unit='Bohr')
         auxcell1 = auxcell.set_geom_(atom_coords, unit='Bohr')
-        opt = int3c2e.SRInt3c2eOpt_v2(cell1, auxcell1, omega).build()
+        opt = int3c2e.SRInt3c2eOpt(cell1, auxcell1, omega).build()
         jaux = opt.contract_dm(dm)
         j2c = int3c2e.sr_int2c2e(auxcell1, omega)[0]
         atom_coords[i,x] -= disp
@@ -112,7 +116,7 @@ C    P
       0.4000000000           1.0000000000
 C    D
       0.1995412500           1.0000000000 ''',
-        'C2':[[0, [.5, 1.]]],
+        'C2': ('unc-weigend', [[0, [.5, 1.]], [1, [.8, 1.]], [3, [.9, 1]]]),
     }
     auxcell.build()
     omega = -0.2
@@ -123,12 +127,12 @@ C    D
     mo_coeff = np.random.rand(nao, nao) - .5
     mo_occ = np.zeros(nao)
     mo_occ[:nocc] = 2
-    opt = int3c2e.SRInt3c2eOpt_v2(cell, auxcell, omega).build()
     dm = (mo_coeff*mo_occ).dot(mo_coeff.T)
-    ek = rhf._jk_energy_per_atom(opt, mo_coeff, mo_occ, j_factor=1, k_factor=1)
-    assert abs(ek.sum(axis=0)).max() < 1e-12
+    dm = tag_array(dm, mo_coeff=mo_coeff, mo_occ=mo_occ)
+    opt = int3c2e.SRInt3c2eOpt(cell, auxcell, omega).build()
+    ek = rhf._jk_energy_per_atom(opt, dm, hermi=1, j_factor=1, k_factor=1)
+    assert abs(ek.sum(axis=0)).max() < 1e-11
 
-    dm = (mo_coeff*mo_occ).dot(mo_coeff.T)
     disp = 1e-3
     atom_coords = cell.atom_coords()
     def eval_jk(i, x, disp):
@@ -177,7 +181,7 @@ C    P
       0.4000000000           1.0000000000
 C    D
       0.1995412500           1.0000000000 ''',
-        'C2':[[0, [.5, 1.]]],
+        'C2': ('unc-weigend', [[0, [.5, 1.]], [1, [.8, 1.]], [3, [.9, 1]]]),
     }
     auxcell.build()
     omega = -0.2
@@ -185,17 +189,18 @@ C    D
     kmesh = [3,1,4]
     kpts = cell.make_kpts(kmesh)
     dm = cp.asarray(np.linalg.inv(cell.pbc_intor('int1e_ovlp', kpts=kpts))*.5)
-    opt = int3c2e.SRInt3c2eOpt_v2(cell, auxcell, omega, kmesh).build()
+    opt = int3c2e.SRInt3c2eOpt(cell, auxcell, omega, kmesh).build()
     ej = krhf._j_energy_per_atom(opt, dm, kpts=kpts)
     assert abs(ej.sum(axis=0)).max() < 1e-12
 
+    dm = SortedGTO.from_cell(cell).apply_C_mat_CT(dm)
     disp = 1e-3
     atom_coords = cell.atom_coords()
     def eval_j(i, x, disp):
         atom_coords[i,x] += disp
         cell1 = cell.set_geom_(atom_coords, unit='Bohr')
         auxcell1 = auxcell.set_geom_(atom_coords, unit='Bohr')
-        opt = int3c2e.SRInt3c2eOpt_v2(cell1, auxcell1, omega, kmesh).build()
+        opt = int3c2e.SRInt3c2eOpt(cell1, auxcell1, omega, kmesh).build()
         jaux = opt.contract_dm(dm, kpts=kpts)
         j2c = int3c2e.sr_int2c2e(auxcell1, omega)[0]
         ref = float(cp.linalg.solve(j2c, jaux).dot(jaux).get()) * .5
@@ -236,7 +241,7 @@ C    P
       0.4000000000           1.0000000000
 C    D
       0.1995412500           1.0000000000 ''',
-        'C2':[[0, [.5, 1.]]],
+        'C2': [[0, [.5, 1.]], [2, [.8, 1.]]],
     }
     auxcell.build()
     omega = -0.2
@@ -251,10 +256,11 @@ C    D
     mo_occ = np.zeros((nkpts, nao))
     mo_occ[:,:nocc] = 2
     dm = cp.einsum('kpi,ki,kqi->kpq', mo_coeff, mo_occ, mo_coeff.conj())
-    opt = int3c2e.SRInt3c2eOpt_v2(cell, auxcell, omega, kmesh).build()
+    dm = tag_array(dm, mo_coeff=mo_coeff, mo_occ=mo_occ)
+    opt = int3c2e.SRInt3c2eOpt(cell, auxcell, omega, kmesh).build()
     j_factor = 1
     k_factor = 1
-    ejk = krhf._jk_energy_per_atom(opt, mo_coeff, mo_occ, kpts=kpts,
+    ejk = krhf._jk_energy_per_atom(opt, dm, kpts, hermi=1,
                                    j_factor=j_factor, k_factor=k_factor)
     assert abs(ejk.sum(axis=0)).max() < 3e-12
 

gpu4pyscf/pbc/df/tests/test_rsdf_builder.py
@@ -84,7 +84,7 @@ def test_kpts():
                       [1, [2., 1.]],
                       [2, [1., 1.]]],
                'C2': 'ccpvdz'},
-        a=np.diag([2.5, 1.9, 2.2])*2)
+        a=np.diag([2.5, 1.9, 2.2])*3)
 
     auxcell = cell.copy()
     auxcell.basis = {
@@ -107,7 +107,7 @@ C    D
     }
     auxcell.build()
     omega = 0.3
-    kmesh = [2,3,1]
+    kmesh = [6,1,1]
     kpts = cell.make_kpts(kmesh)
     gpu_dat, dat_neg = build_cderi(cell, auxcell, kpts, omega=omega)
 