Fix grid dimension overflow bug in ft_ao in fftdf pseudopotential evaluation (#626)
* Fix grid dimension overflow bug in ft_ao in fftdf pseudopotential evaluation

* Fix linter error

* Also avoid int32 overflow

gpu4pyscf/pbc/df/ft_ao.py
@@ -75,7 +75,8 @@ def ft_ao(cell, Gv, shls_slice=None, b=None,
           sort_cell=True):
     '''Analytical Fourier transform basis functions on Gv grids.
 
-    If the sorted_cell in the input is specified, the transform
+    If the sort_cell in the input is specified, the ao is evaluated on a sorted Cartesian basis,
+    and then transformed back to original basis.
     '''
     assert shls_slice is None
     if sort_cell:
@@ -95,11 +96,12 @@ def ft_ao(cell, Gv, shls_slice=None, b=None,
         _bas.data.ptr, _env.data.ptr, ao_loc_gpu.data.ptr, 1, 1, 0,
     )
     ngrids = len(Gv)
+    assert ngrids < np.iinfo(np.int32).max, "possible int32 overflow"
     GvT = (asarray(Gv).T + asarray(kpt[:,None])).ravel()
     GvT = cp.append(GvT, cp.zeros(THREADS))
     nao_cart = ao_loc_cpu[-1]
     out = cp.empty((nao_cart, ngrids), dtype=np.complex128)
-    libpbc.build_ft_ao(
+    err = libpbc.build_ft_ao(
         ctypes.cast(out.data.ptr, ctypes.c_void_p),
         ctypes.byref(envs), ctypes.c_int(ngrids),
         ctypes.cast(GvT.data.ptr, ctypes.c_void_p),
@@ -107,6 +109,8 @@ def ft_ao(cell, Gv, shls_slice=None, b=None,
         sorted_cell._bas.ctypes, ctypes.c_int(sorted_cell.nbas),
         sorted_cell._env.ctypes
     )
+    if err != 0:
+        raise RuntimeError('build_ft_ao failed')
     if sort_cell:
         out = out.T.dot(asarray(coeff))
     else: