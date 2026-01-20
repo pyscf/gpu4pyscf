70334e2cb058cfa5fbd13377212697bcf1171df2
puzhichen.996 <puzhichen.996@bytedance.com>
2026-01-20 14:11:42 +0800

    add a trial routine

--- gpu4pyscf/properties/c8.py
+++ gpu4pyscf/properties/c8.py
@@ -23,22 +23,74 @@ from gpu4pyscf.lib import logger
 
 import numpy as np
 
+# def _get_g_osc(td):
+#     e_exc = td.e
+#     q = td.transition_quadrupole()
+    
+#     q_xx = q[:, 0, 0]
+#     q_yy = q[:, 1, 1]
+#     q_zz = q[:, 2, 2]
+#     q_xy = q[:, 0, 1]
+#     q_xz = q[:, 0, 2]
+#     q_yz = q[:, 1, 2]
+    
+
+#     q_sum_sq_cartesian = (q_xx**2 + q_yy**2 + q_zz**2 + 
+#                           q_xy**2 + q_xz**2 + q_yz**2)
+    
+#     g_osc = (2.0 / 6.0) * e_exc * q_sum_sq_cartesian
+    
+#     return g_osc
+
+# def _get_g_osc(td):
+#     e_exc = td.e
+#     q = td.transition_quadrupole()
+
+#     # trace = q[:, 0, 0] + q[:, 1, 1] + q[:, 2, 2]
+#     # q = (3*q - trace[:, None, None]*np.eye(3)[None,])
+    
+#     q_sum = 0.0
+#     for i in range(3):
+#         for j in range(3):
+#             for k in range(3):
+#                 for l in range(3):
+#                     q_sum += q[:, i, j] * q[:, k, l]
+    
+#     g_osc = (2.0 / 6.0) * e_exc * q_sum
+    
+#     return g_osc
+
 def _get_g_osc(td):
     e_exc = td.e
     q = td.transition_quadrupole()
+    # trace = q[:, 0, 0] + q[:, 1, 1] + q[:, 2, 2]
+    # q = (3 * q - trace[:, None, None]*np.eye(3)[None,])
     
-    q_xx = q[:, 0, 0]
-    q_yy = q[:, 1, 1]
-    q_zz = q[:, 2, 2]
-    q_xy = q[:, 0, 1]
-    q_xz = q[:, 0, 2]
-    q_yz = q[:, 1, 2]
-    
+    # q_xx = q[:, 0, 0]
+    # q_yy = q[:, 1, 1]
+    # q_zz = q[:, 2, 2]
+    # q_xy = q[:, 0, 1]
+    # q_xz = q[:, 0, 2]
+    # q_yz = q[:, 1, 2]
+    # coeff_m1 = np.sqrt(15 / (8 * np.pi))
+    # coeff_m2 = 0.25 * np.sqrt(15 / (2 * np.pi))
 
-    q_sum_sq_cartesian = (q_xx**2 + q_yy**2 + q_zz**2 + 
-                          q_xy**2 + q_xz**2 + q_yz**2)
-    
-    g_osc = (2.0 / 6.0) * e_exc * q_sum_sq_cartesian
+    # q20 = 0.25 * np.sqrt(5 / np.pi) * (2 * q_zz - q_xx - q_yy)
+    # q21  = -coeff_m1 * (q_xz + 1j * q_yz)
+    # q21_m = coeff_m1 * (q_xz - 1j * q_yz)
+    # q22  = coeff_m2 * ( (q_xx - q_yy) + 2j * q_xy )
+    # q22_m = coeff_m2 * ( (q_xx - q_yy) - 2j * q_xy )
+    # q_sum_sq_cartesian = q20*q20.conj() + q21*q21.conj() + q22*q22.conj() + q21_m*q21_m.conj() + q22_m*q22_m.conj()
+
+    # q20 = q_zz 
+    # q21c = 2/np.sqrt(3) * q_xz
+    # q21s = 2/np.sqrt(3) * q_yz
+    # q22c = 1/np.sqrt(3) * (q_xx - q_yy)
+    # q22s = 2/np.sqrt(3) * q_xy
+
+    # q_sum_sq_cartesian = q20*q20.conj() + q21c*q21c.conj() + q22c*q22c.conj() + q21s*q21s.conj() + q22s*q22s.conj()
+
+    g_osc = (2.0 / 5.0) * e_exc * q_sum_sq_cartesian.real#*4*np.pi/3
     
     return g_osc
 
@@ -47,7 +99,28 @@ def _get_g_osc(td):
 #     q = td.transition_quadrupole()
 
 #     trace = q[:, 0, 0] + q[:, 1, 1] + q[:, 2, 2]
-#     q = 0.5*(3*q - trace[:, None, None]*np.eye(3)[None,])
+#     q = (3*q - trace[:, None, None]*np.eye(3)[None,])*0.5
+    
+#     q_xx = q[:, 0, 0]
+#     q_yy = q[:, 1, 1]
+#     q_zz = q[:, 2, 2]
+#     q_xy = q[:, 0, 1]
+#     q_xz = q[:, 0, 2]
+#     q_yz = q[:, 1, 2]
+    
+#     q_sum_sq_cartesian = (q_xx*q_xx + q_yy*q_yy + q_zz*q_zz + 
+#                           2*q_xy*q_xy + 2*q_xz*q_xz + 2*q_yz*q_yz )
+    
+#     g_osc = (2.0 / 5.0) * e_exc * q_sum_sq_cartesian
+    
+#     return g_osc
+
+# def _get_g_osc(td):
+#     e_exc = td.e
+#     q = td.transition_quadrupole()
+
+#     trace = q[:, 0, 0] + q[:, 1, 1] + q[:, 2, 2]
+#     q = (3*q - trace[:, None, None]*np.eye(3)[None,])
     
 #     q_xx = q[:, 0, 0]
 #     q_yy = q[:, 1, 1]
@@ -59,7 +132,7 @@ def _get_g_osc(td):
 #     q_sum_sq_cartesian = (q_xx*q_xx + q_yy*q_yy + q_zz*q_zz + 
 #                           q_xy*q_xy*2 + q_xz*q_xz*2 + q_yz*q_yz*2 - 1/3*(q_xx*q_yy + q_xx*q_zz + q_yy*q_zz))
     
-#     g_osc = (2.0 / 20.0) * e_exc * q_sum_sq_cartesian
+#     g_osc = (2.0 / 5.0) * e_exc * q_sum_sq_cartesian
     
 #     return g_osc
 
