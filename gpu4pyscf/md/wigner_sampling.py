3458c82ef38231379345ff68abc1b39b55910260
Qiming Sun <qiming.sun@bytedance.com>
2026-02-06 17:09:54 +0800

    Add tests for wigner sampling

--- gpu4pyscf/md/wigner_sampling.py
+++ gpu4pyscf/md/wigner_sampling.py
@@ -1,16 +1,34 @@
-import numpy as np
-import random
-from scipy.special import laguerre
+# Copyright 2021-2025 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-def wignerfunc(mu, temp):
-    max_pop, max_nlevel = 0.9999, 150
-    ex = mu / (0.69503 * temp)  # vibrational temperature: ex=h*c*mu/(kb*T), 0.69503 convert cm-1 to K
+import numpy as np
+from scipy.special import laguerre
+from pyscf.data.nist import HARTREE2WAVENUMBER, AMU2AU, BOHR, PLANCK, BOLTZMANN, LIGHT_SPEED_SI
+
+wavenumber2Kelvin = 100 * (PLANCK * LIGHT_SPEED_SI) / BOLTZMANN
+
+def wignerfunc(mu, temp, max_pop=0.9999, max_nlevel=150, seed=None):
+    ex = mu / temp * wavenumber2Kelvin  # vibrational temperature: ex=h*c*mu/(kb*T), 0.69503 convert cm-1 to K
     pop = []
 
     while np.sum(pop) < max_pop and len(pop) < max_nlevel:
         _pop = float(np.exp(-1 * ex * len(pop)) * (1 - np.exp(-1 * ex)))
         pop.append(_pop)
     pop.append(1 - np.sum(pop))
+
+    if seed is not None:
+        np.random.seed(seed)
 
     while True:
         random_state = np.random.choice(len(pop), p=pop)  # random generate a state
@@ -23,29 +41,59 @@ def wignerfunc(mu, temp):
     return float(q), float(p)
 
 
-def wigner(temp, freqs, xyz, vib):
-
+def wigner(temp, freqs, xyz, vib, seed=None):
     nfreq = len(freqs)
     natom = len(xyz)
 
-    mu_to_hartree = 4.55633518e-6  # 1 cm-1  = h*c/Eh = 4.55633518e-6 au
-    ma_to_amu = 1822.88852  # 1 g/mol = 1/Na*me*1000 = 1822.88852 amu
-    bohr_to_angstrom = 0.529177249  # 1 Bohr  = 0.529177249 Angstrom
+    mu_to_hartree = 1./HARTREE2WAVENUMBER  # 1 cm-1  = h*c/Eh = 4.55633518e-6 au
+    ma_to_amu = AMU2AU  # 1 g/mol = 1/Na*me*1000 = 1822.88852 amu
+    bohr_to_angstrom = BOHR  # 1 Bohr  = 0.529177249 Angstrom
 
-    q_p = np.array([wignerfunc(i, temp) for i in freqs])  # generates update coordinates and momenta pairs Q and P
+    q_p = np.array([wignerfunc(i, temp, seed=seed) for i in freqs])  # generates update coordinates and momenta pairs Q and P
 
     q = q_p[:, 0].reshape((nfreq, 1))  # first column is Q
 
-    q *= 1 / np.sqrt(freqs * mu_to_hartree * ma_to_amu)  # convert coordinates from m to Bohr
+    q *= 1 / np.sqrt(freqs[:,None] * mu_to_hartree * ma_to_amu)  # convert coordinates from m to Bohr
     qvib = np.array([np.ones((natom, 3)) * i for i in q])  # generate identity array to expand Q
     qvib = np.sum(vib * qvib, axis=0)  # sum sampled structure over all modes
     newc = (xyz + qvib) * bohr_to_angstrom  # cartesian coordinates in Angstrom
 
     p = q_p[:, 1].reshape((nfreq, 1))  # second column is P
-    p *= np.sqrt(freqs * mu_to_hartree / ma_to_amu)  # convert velocity from m/s to Bohr/au
+    p *= np.sqrt(freqs[:,None] * mu_to_hartree / ma_to_amu)  # convert velocity from m/s to Bohr/au
     pvib = np.array([np.ones((natom, 3)) * i for i in p])  # generate identity array to expand P
     velo = np.sum(vib * pvib, axis=0)  # sum sampled velocity over all modes in Bohr/au
  
     initcond = np.concatenate((newc, velo), axis=1)
 
     return initcond
+
+def wigner_samples(temp, freqs, xyz, vib, samples, seed=None):
+    '''
+    Wigner sampling to produce initial conditions for MD simulations.
+    Ref. https://pubs.rsc.org/en/content/articlelanding/2023/cp/d3cp01007d
+
+    Args:
+        temp:
+            Temperature in Kelvin
+        freqs:
+            Vibrational freqencies in cm-1
+        xyz:
+            Coordinates in atomic unit
+        vib:
+            Normal modes in atomic unit
+        samples:
+            Number of initial samples to generate
+    '''
+    if seed is not None:
+        np.random.seed(seed)
+    valid = []
+    temperature = 300
+    while len(valid) < samples:
+        initcond = wigner(temperature, freqs, xyz, vib)
+        p = initcond[:,:3]
+        distance = np.linalg.norm(p[:,None] - p, axis=-1)
+        distance = distance[np.tril_indices(len(p), -1)]
+        # filter out unreasonable geometries
+        if distance.min() > 0.7:
+            valid.append(initcond)
+    return np.array(valid)
