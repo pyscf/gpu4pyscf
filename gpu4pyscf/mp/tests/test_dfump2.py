b45fe958cb4ba7c1e6f7e60a56e6f64ad190388a
ajz34 <ajz34@outlook.com>
2026-02-22 11:20:47 +0800

    fix test of dfump2 in mf -> mf_gpu

--- gpu4pyscf/mp/tests/test_dfump2.py
+++ gpu4pyscf/mp/tests/test_dfump2.py
@@ -39,34 +39,36 @@ def tearDownModule():
 
 class KnownValues(unittest.TestCase):
     def test_dfump2(self):
-        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf, auxbasis='def2-TZVPP-ri').run()
+        mf_gpu = mf.to_gpu()
+        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf_gpu, auxbasis='def2-TZVPP-ri').run()
         print(mp.e_corr_os, mp.e_corr_ss, mp.e_corr)
         e_corr_ref = -0.2276193004374617
         self.assertAlmostEqual(mp_gpu.e_corr_os, -0.17417641954815585, 8)
         self.assertAlmostEqual(mp_gpu.e_corr_ss, -0.05344288088930613, 8)
         self.assertAlmostEqual(mp_gpu.e_corr, e_corr_ref, 8)
 
-        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf, auxbasis='def2-TZVPP-ri').run(j2c_decomp_alg='eig')
+        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf_gpu, auxbasis='def2-TZVPP-ri').run(j2c_decomp_alg='eig')
         self.assertAlmostEqual(mp_gpu.e_corr, e_corr_ref, 8)
 
-        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf, auxbasis='def2-TZVPP-ri').run(j3c_backend='vhfopt')
+        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf_gpu, auxbasis='def2-TZVPP-ri').run(j3c_backend='vhfopt')
         self.assertAlmostEqual(mp_gpu.e_corr, e_corr_ref, 8)
 
-        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf, auxbasis='def2-TZVPP-ri').run(fp_type='FP32')
+        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf_gpu, auxbasis='def2-TZVPP-ri').run(fp_type='FP32')
         self.assertAlmostEqual(mp_gpu.e_corr, e_corr_ref, 5)
 
-        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf, auxbasis='def2-TZVPP-ri').run(with_t2=True)
+        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf_gpu, auxbasis='def2-TZVPP-ri').run(with_t2=True)
         for t2_gpu, t2_cpu in zip(mp_gpu.t2, mp.t2):
             self.assertTrue(cp.allclose(t2_gpu, t2_cpu, atol=1e-6))
 
     def test_dfump2_frozen(self):
+        mf_gpu = mf.to_gpu()
         mp_frz = pyscf.mp.ump2.UMP2(mf).run(frozen=[1, 2])
-        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf, auxbasis='def2-TZVPP-ri').run(frozen=mp_frz.frozen)
+        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf_gpu, auxbasis='def2-TZVPP-ri').run(frozen=mp_frz.frozen)
         self.assertAlmostEqual(mp_gpu.e_corr, -0.0637475985546614, 8)
         self.assertAlmostEqual(mp_gpu.e_corr, mp_frz.e_corr, 8)
 
         mp_frz = pyscf.mp.ump2.UMP2(mf).run(frozen=[[0, 1], [1, 2]])
-        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf, auxbasis='def2-TZVPP-ri').run(frozen=mp_frz.frozen)
+        mp_gpu = gpu4pyscf.mp.dfump2.DFUMP2(mf_gpu, auxbasis='def2-TZVPP-ri').run(frozen=mp_frz.frozen)
         self.assertAlmostEqual(mp_gpu.e_corr, -0.08095767866785737, 8)
         self.assertAlmostEqual(mp_gpu.e_corr, mp_frz.e_corr, 8)
 
