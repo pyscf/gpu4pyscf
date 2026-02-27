8d690f9034c1365dfb6e8f8ba616fa0f89d08d51
Qiming Sun <qiming.sun@bytedance.com>
2026-02-27 08:20:57 -0800

    Bugfixes for evaluating multiple sets of derivatives in TDDFT (#668)
    * Bugfixes for evaluating multiple sets of derivatives

--- gpu4pyscf/df/grad/tdrhf.py
+++ gpu4pyscf/df/grad/tdrhf.py
@@ -592,7 +592,7 @@ def _j_energies_per_atom(int3c2e_opt, dm_pairs, j_factor, hermi=None, verbose=No
         ctypes.cast(ksh_offsets_gpu.data.ptr, ctypes.c_void_p),
         ctypes.cast(gout_stride.data.ptr, ctypes.c_void_p),
         lib.c_null_ptr(), ctypes.c_int(0),
-        ctypes.c_int(nao), ctypes.c_int(0),
+        ctypes.c_int(dms.shape[-1]), ctypes.c_int(0),
         ctypes.c_int(naux), ctypes.c_int(mol.natm))
     if err != 0:
         raise RuntimeError('int3c2e_ejk_ip1 failed')
