3920ff160728f47f55ae949d0c92bbe47a5868aa
puzhichen.996 <puzhichen.996@bytedance.com>
2026-03-03 11:12:12 +0800

    add unit tests for tddft and tdhf

--- gpu4pyscf/df/tests/test_df_tdrks_nac_batch.py
+++ gpu4pyscf/df/tests/test_df_tdrks_nac_batch.py
@@ -292,6 +292,69 @@ class KnownValues(unittest.TestCase):
         g1.kernel()
 
         assert abs(np.abs(nac_test.grad_result) - np.abs(g1.de)).max() < 1e-9
+
+    def test_df_nac_tddft_singlet_b3lyp(self):
+        """
+        density fitting only
+        """
+        mf = dft.RKS(mol, xc='b3lyp').density_fit().to_gpu()
+        mf.kernel()
+        td = mf.TDDFT().set(nstates=5)
+        td.kernel()
+
+        nac_test = td.nac_gradient_method()
+        nac_test.states=(1,2,3)
+        nac_test.kernel()
+
+        nac1 = td.nac_method()
+        nac1.states=(1,2)
+        nac1.kernel()
+        assert abs(np.abs(nac_test.results[(1,2)]['de']) - np.abs(nac1.de)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(1,2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(1,2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(1,2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-9
+
+        nac1 = td.nac_method()
+        nac1.states=(1,3)
+        nac1.kernel()
+        assert abs(np.abs(nac_test.results[(1,3)]['de']) - np.abs(nac1.de)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(1,3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(1,3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(1,3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-9
+
+        nac1 = td.nac_method()
+        nac1.states=(2,3)
+        nac1.kernel()
+        assert abs(np.abs(nac_test.results[(2,3)]['de']) - np.abs(nac1.de)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(2,3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(2,3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(2,3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-9
+
+    def test_df_nac_grad_tddft_singlet_b3lyp(self):
+        mf = dft.RKS(mol, xc='b3lyp').density_fit().to_gpu()
+        mf.kernel()
+        td = mf.TDDFT().set(nstates=5)
+        td.kernel()
+
+        nac_test = td.nac_gradient_method()
+        nac_test.states=(0,1,2,3)
+        nac_test.grad_state = 1
+        nac_test.kernel()
+
+        nac1 = td.nac_method()
+        nac1.states=(1,2)
+        nac1.kernel()
+
+        assert abs(np.abs(nac_test.results[(1,2)]['de']) - np.abs(nac1.de)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(1,2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(1,2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(1,2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-9
+
+        g1 = td.nuc_grad_method()
+        g1.state=1
+        g1.kernel()
+
+        assert abs(np.abs(nac_test.grad_result) - np.abs(g1.de)).max() < 1e-9
     
 
 if __name__ == "__main__":
