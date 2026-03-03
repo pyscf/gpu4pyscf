a67bd1bcce3329869cf46dd2b1156174eaecc888
felixmiaomiao <163409570+felixmiaomiao@users.noreply.github.com>
2026-03-03 16:24:48 +0800

    Add FSSH Tully simulation example
    This script demonstrates the FSSH simulation using the Tully model, including initialization and kernel execution.

---
+++ examples/md/01-fssh_tully.py
@@ -0,0 +1,28 @@
+# Copyright 2021-2025 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+'''
+This example demonstrates the FSSH simualtion with Tully model
+'''
+
+import numpy as np
+import pyscf
+from gpu4pyscf.md.fssh_tully import FSSH_Tully
+
+fssh = FSSH_Tully('sac',2000)
+fssh.cur_state = 0
+fssh.decoherence = False
+fssh.nsteps = 300
+fssh.kernel(np.array([[-10]]), np.array([[v]]) / 2000, np.array([1.0,0.0]))
