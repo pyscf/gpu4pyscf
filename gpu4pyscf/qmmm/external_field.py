Fix the wrong sign for external field

gpu4pyscf/qmmm/external_field.py
@@ -90,7 +90,7 @@ class EXTFSCF(EXTF):
         if self.electric_field is not None:
             with mol.with_common_orig(self.origin):
                 dipole_integral = cp.asarray(mol.intor('int1e_r'))
-            h1e -= cp.einsum('d,dij->ij', self.electric_field, dipole_integral)
+            h1e += cp.einsum('d,dij->ij', self.electric_field, dipole_integral)
 
         return h1e
 
@@ -101,7 +101,7 @@ class EXTFSCF(EXTF):
         nuclear_coords = self.mol.atom_coords()
         if self.electric_field is not None:
             nuclear_dipole = nuclear_charges @ (nuclear_coords - self.origin[None, :])
-            nuc += float(nuclear_dipole @ self.electric_field.get())
+            nuc -= float(nuclear_dipole @ self.electric_field.get())
         return nuc
 
     def Gradients(self):
@@ -161,7 +161,7 @@ class EXTFGrad:
                 dipole_integral_derivative = mol.intor('int1e_irp').reshape(3, 3, mol.nao, mol.nao).transpose(0,1,3,2)
                 dipole_integral_derivative = cp.asarray(dipole_integral_derivative)
 
-            dhcore += cp.einsum('Edij,E->dij', dipole_integral_derivative, self.base.electric_field)
+            dhcore -= cp.einsum('Edij,E->dij', dipole_integral_derivative, self.base.electric_field)
         return dhcore
 
     def grad_nuc(self, mol=None, atmlst=None):
@@ -171,7 +171,7 @@ class EXTFGrad:
         nuclear_charges = mol.atom_charges()
         # nuclear_coords = mol.atom_coords()
         if self.base.electric_field is not None:
-            g_nuc += np.einsum('q,E->qE', nuclear_charges, self.base.electric_field.get())
+            g_nuc -= np.einsum('q,E->qE', nuclear_charges, self.base.electric_field.get())
         return g_nuc
 
 # Inject EXTF interface wrapper to other modules

gpu4pyscf/qmmm/tests/test_external_field.py
@@ -55,8 +55,8 @@ def emulate_field_with_charges(field, distance = 10000, origin = np.array([0,0,0
     field_norm = np.linalg.norm(field)
     field_direction = field / field_norm
     coords = np.empty([2,3])
-    coords[0] = origin + field_direction * distance
-    coords[1] = origin - field_direction * distance
+    coords[0] = origin - field_direction * distance
+    coords[1] = origin + field_direction * distance
     charges = np.empty(2)
     charges[0] =  field_norm / 2 * distance**2
     charges[1] = -field_norm / 2 * distance**2
@@ -185,7 +185,7 @@ class KnownValues(unittest.TestCase):
         mf = external_field.add_external_field(mf, field)
         energy_with_field = mf.kernel()
         assert mf.converged
-        assert abs(energy_with_field - -75.95176870367526) < 1e-10
+        assert abs(energy_with_field - -75.96828327992773) < 1e-10
 
         mf = mf.undo_external_field()
         energy_without_field = mf.kernel()