abf6345bc8088430f66ef8a777bd3bddd590eac3
puzhichen.996 <puzhichen.996@bytedance.com>
2026-03-12 11:55:01 +0800

    lower 2 unit test threshoulds

--- gpu4pyscf/df/tests/test_df_tdrks_ris_nac_batch.py
+++ gpu4pyscf/df/tests/test_df_tdrks_ris_nac_batch.py
@@ -124,28 +124,29 @@ class KnownValues(unittest.TestCase):
         nac1.states=(1, 2)
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(1, 2)]['de']) - np.abs(nac1.de)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-9
+        # ! low accuracy due to instability of TDDFT gradient (seen from divergence in github)
+        assert abs(np.abs(nac_test.results[(1, 2)]['de']) - np.abs(nac1.de)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
 
         nac1 = td.nac_method()
         nac1.states=(1, 3)
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(1, 3)]['de']) - np.abs(nac1.de)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(1, 3)]['de']) - np.abs(nac1.de)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
 
         nac1 = td.nac_method()
         nac1.states=(2, 3)
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(2, 3)]['de']) - np.abs(nac1.de)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(2, 3)]['de']) - np.abs(nac1.de)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
 
     def test_df_nac_grad_tda_singlet_b3lyp(self):
         mf = dft.RKS(mol, xc='b3lyp').density_fit().to_gpu()
@@ -329,28 +330,29 @@ class KnownValues(unittest.TestCase):
         nac1.states=(1, 2)
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(1, 2)]['de']) - np.abs(nac1.de)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-9
+        # ! low accuracy due to instability of TDDFT gradient (seen from divergence in github)
+        assert abs(np.abs(nac_test.results[(1, 2)]['de']) - np.abs(nac1.de)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
 
         nac1 = td.nac_method()
         nac1.states=(1, 3)
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(1, 3)]['de']) - np.abs(nac1.de)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(1, 3)]['de']) - np.abs(nac1.de)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
 
         nac1 = td.nac_method()
         nac1.states=(2, 3)
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(2, 3)]['de']) - np.abs(nac1.de)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-9
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-9
+        assert abs(np.abs(nac_test.results[(2, 3)]['de']) - np.abs(nac1.de)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
 
     def test_df_nac_grad_tddft_singlet_b3lyp(self):
         mf = dft.RKS(mol, xc='b3lyp').density_fit().to_gpu()
