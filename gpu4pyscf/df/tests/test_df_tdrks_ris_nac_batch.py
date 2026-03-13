260c4858db6ea26ffec708a8afe0ec4971f6a9c3
Qiming Sun <qiming.sun@bytedance.com>
2026-03-13 12:17:42 -0700

    Adjust grad-nac batch tests

--- gpu4pyscf/df/tests/test_df_tdrks_ris_nac_batch.py
+++ gpu4pyscf/df/tests/test_df_tdrks_ris_nac_batch.py
@@ -125,28 +125,28 @@ class KnownValues(unittest.TestCase):
         nac1.kernel()
 
         # ! low accuracy due to instability of TDDFT gradient (seen from divergence in github)
-        assert abs(np.abs(nac_test.results[(1, 2)]['de']) - np.abs(nac1.de)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 2)]['de']) - np.abs(nac1.de)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-6
 
         nac1 = td.nac_method()
         nac1.states=(1, 3)
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(1, 3)]['de']) - np.abs(nac1.de)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 3)]['de']) - np.abs(nac1.de)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-6
 
         nac1 = td.nac_method()
         nac1.states=(2, 3)
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(2, 3)]['de']) - np.abs(nac1.de)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(2, 3)]['de']) - np.abs(nac1.de)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-6
 
     def test_df_nac_grad_tda_singlet_b3lyp(self):
         mf = dft.RKS(mol, xc='b3lyp').density_fit().to_gpu()
@@ -165,9 +165,9 @@ class KnownValues(unittest.TestCase):
         nac1.states=(1,2)
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(1,2)]['de']) - np.abs(nac1.de)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1,2)]['de']) - np.abs(nac1.de)).max() < 1e-6
         assert abs(np.abs(nac_test.results[(1,2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-6
-        assert abs(np.abs(nac_test.results[(1,2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1,2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-6
         assert abs(np.abs(nac_test.results[(1,2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-6
 
         g1 = td.nuc_grad_method()
@@ -237,9 +237,9 @@ class KnownValues(unittest.TestCase):
 
         # ! NOTE: Low accuracy arising from batched Z-vector solver.
         assert abs(np.abs(nac_test.results[(1,2)]['de']) - np.abs(nac1.de)).max() < 1e-8
-        assert abs(np.abs(nac_test.results[(1,2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1,2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-6
         assert abs(np.abs(nac_test.results[(1,2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-8
-        assert abs(np.abs(nac_test.results[(1,2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1,2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-6
 
         g1 = td.nuc_grad_method()
         g1.state=1
@@ -331,28 +331,28 @@ class KnownValues(unittest.TestCase):
         nac1.kernel()
 
         # ! low accuracy due to instability of TDDFT gradient (seen from divergence in github)
-        assert abs(np.abs(nac_test.results[(1, 2)]['de']) - np.abs(nac1.de)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 2)]['de']) - np.abs(nac1.de)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-6
 
         nac1 = td.nac_method()
         nac1.states=(1, 3)
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(1, 3)]['de']) - np.abs(nac1.de)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1, 3)]['de']) - np.abs(nac1.de)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(1, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-6
 
         nac1 = td.nac_method()
         nac1.states=(2, 3)
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(2, 3)]['de']) - np.abs(nac1.de)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
-        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(2, 3)]['de']) - np.abs(nac1.de)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-6
+        assert abs(np.abs(nac_test.results[(2, 3)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-6
 
     def test_df_nac_grad_tddft_singlet_b3lyp(self):
         mf = dft.RKS(mol, xc='b3lyp').density_fit().to_gpu()
@@ -373,9 +373,9 @@ class KnownValues(unittest.TestCase):
 
         # ! NOTE: Low accuracy arising from batched Z-vector solver.
 
-        assert abs(np.abs(nac_test.results[(1,2)]['de']) - np.abs(nac1.de)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1,2)]['de']) - np.abs(nac1.de)).max() < 1e-6
         assert abs(np.abs(nac_test.results[(1,2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-6
-        assert abs(np.abs(nac_test.results[(1,2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1,2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-6
         assert abs(np.abs(nac_test.results[(1,2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-6
 
         g1 = td.nuc_grad_method()
@@ -457,9 +457,9 @@ class KnownValues(unittest.TestCase):
         nac1.ris_zvector_solver = True
         nac1.kernel()
 
-        assert abs(np.abs(nac_test.results[(1,2)]['de']) - np.abs(nac1.de)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1,2)]['de']) - np.abs(nac1.de)).max() < 1e-6
         assert abs(np.abs(nac_test.results[(1,2)]['de_scaled']) - np.abs(nac1.de_scaled)).max() < 1e-6
-        assert abs(np.abs(nac_test.results[(1,2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-7
+        assert abs(np.abs(nac_test.results[(1,2)]['de_etf']) - np.abs(nac1.de_etf)).max() < 1e-6
         assert abs(np.abs(nac_test.results[(1,2)]['de_etf_scaled']) - np.abs(nac1.de_etf_scaled)).max() < 1e-6
 
         g1 = td.nuc_grad_method()
