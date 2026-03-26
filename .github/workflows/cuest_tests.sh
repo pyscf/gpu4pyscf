647dc63bb4f53a801fd5647777d87b3728feb7ad
Henry Wang <henrywang0142857@gmail.com>
2026-03-25 17:32:35 -0700

    Allow test bash to run anywhere

--- .github/workflows/cuest_tests.sh
+++ .github/workflows/cuest_tests.sh
@@ -1,6 +1,8 @@
 # Given our CI machines are very old and CUDA 13 has already drop the support, we do not make a CI job for cuEST.
 
-pytest ../../gpu4pyscf/lib/tests/test_cuest_wrapper.py
+MY_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
+pytest ${MY_DIR}/../../gpu4pyscf/lib/tests/test_cuest_wrapper.py
 
 # Notice there's a special test: test_K_spherical_mo_input_really_low_precision
 # If it failes because of the last check (an error message will be printed),
