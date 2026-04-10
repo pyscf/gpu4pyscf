Fix a bug when computing vv10 grid response gradient with natm==1

gpu4pyscf/grad/tests/test_grid_response.py
@@ -80,7 +80,7 @@ def numerical_gradient(mf):
     return numerical_gradient
 
 class KnownValues(unittest.TestCase):
-    def test_grids_response_spherical(self):
+    def test_grid_response_spherical(self):
         mf = rks.RKS(mol_sph, xc = 'PBE')
         mf = mf.density_fit()
         mf.grids.atom_grid = atom_grid_loose
@@ -103,7 +103,7 @@ class KnownValues(unittest.TestCase):
 
         assert np.abs(np.max(test_gradient - ref_gradient)) < 1e-7
 
-    def test_grids_response_cartesian(self):
+    def test_grid_response_cartesian(self):
         mf = rks.RKS(mol_cart, xc = 'r2SCAN')
         mf = mf.density_fit()
         mf.grids.atom_grid = atom_grid_loose
@@ -125,6 +125,29 @@ class KnownValues(unittest.TestCase):
         test_gradient = gobj.kernel()
 
         assert abs(test_gradient - ref_gradient).max() < 3e-6
+
+    def test_grid_response_one_atom(self):
+        mol = pyscf.M(
+            atom = "Na 10 100 1000",
+            basis = "def2-svp",
+            charge = 1,
+            verbose = 0,
+        )
+
+        mf = rks.RKS(mol, xc = 'wB97M-V').density_fit(auxbasis = "def2-universal-jkfit")
+        mf.grids.atom_grid = (2,6)
+        mf.conv_tol = 1e-12
+
+        ref_gradient = np.zeros((1,3))
+
+        mf.kernel()
+        assert mf.converged
+
+        gobj = mf.Gradients()
+        gobj.grid_response = True
+        test_gradient = gobj.kernel()
+
+        assert np.abs(np.max(test_gradient - ref_gradient)) < 1e-9
 
 if __name__ == "__main__":
     print("Full Tests for grid response")