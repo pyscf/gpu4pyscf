Correct result following vv10 hessian paper. Performance tuning in progress

gpu4pyscf/lib/cupy_helper.py
@@ -1160,27 +1160,47 @@ def batched_vec3_norm2(batched_vec3):
     assert type(batched_vec3) is cupy.ndarray
     assert batched_vec3.dtype == cupy.float64
     assert batched_vec3.ndim == 2
-    assert batched_vec3.shape[1] == 3
+    assert batched_vec3.shape[0] == 3 or batched_vec3.shape[1] == 3
     assert batched_vec3.flags.c_contiguous
 
-    fn_name = "vec3_norm2_kernel"
-    if fn_name not in _kernel_registery:
-        kernel_code = r'''
-            extern "C" __global__
-            void vec3_norm2_kernel(const double* __restrict__ vec3, double* __restrict__ norm2, const int n) {
-                const int i = blockDim.x * blockIdx.x + threadIdx.x;
-                if (i >= n) return;
-                const double x = vec3[i * 3 + 0];
-                const double y = vec3[i * 3 + 1];
-                const double z = vec3[i * 3 + 2];
-                norm2[i] = x*x + y*y + z*z;
-            }
-        '''
-        _kernel_registery[fn_name] = cupy.RawKernel(kernel_code, fn_name)
+    order = "c" if batched_vec3.shape[1] == 3 else "f"
+
+    n = batched_vec3.shape[0] if order == "c" else batched_vec3.shape[1]
+    assert n != 3, "Ambiguous array order, cannot determine if the array is C or Fortran order from the shape"
+    assert n * 3 < np.iinfo(np.int32).max
+
+    if order == "c":
+        fn_name = "vec3_norm2_kernel_c_order"
+        if fn_name not in _kernel_registery:
+            kernel_code = r'''
+                extern "C" __global__
+                void vec3_norm2_kernel_c_order(const double* __restrict__ vec3, double* __restrict__ norm2, const int n) {
+                    const int i = blockDim.x * blockIdx.x + threadIdx.x;
+                    if (i >= n) return;
+                    const double x = vec3[i * 3 + 0];
+                    const double y = vec3[i * 3 + 1];
+                    const double z = vec3[i * 3 + 2];
+                    norm2[i] = x*x + y*y + z*z;
+                }
+            '''
+            _kernel_registery[fn_name] = cupy.RawKernel(kernel_code, fn_name)
+    else:
+        fn_name = "vec3_norm2_kernel_f_order"
+        if fn_name not in _kernel_registery:
+            kernel_code = r'''
+                extern "C" __global__
+                void vec3_norm2_kernel_f_order(const double* __restrict__ vec3, double* __restrict__ norm2, const int n) {
+                    const int i = blockDim.x * blockIdx.x + threadIdx.x;
+                    if (i >= n) return;
+                    const double x = vec3[n * 0 + i];
+                    const double y = vec3[n * 1 + i];
+                    const double z = vec3[n * 2 + i];
+                    norm2[i] = x*x + y*y + z*z;
+                }
+            '''
+            _kernel_registery[fn_name] = cupy.RawKernel(kernel_code, fn_name)
     kernel = _kernel_registery[fn_name]
 
-    n = batched_vec3.shape[0]
-    assert n < np.iinfo(np.int32).max
     batched_norm2 = cupy.zeros(n, dtype = cupy.float64)
     kernel(((n + 1024 - 1) // 1024,), (1024,), (batched_vec3, batched_norm2, cupy.int32(n)))
 