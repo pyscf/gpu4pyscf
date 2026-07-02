Fix numerical stability issue

gpu4pyscf/df/grad/rhf.py
@@ -146,23 +146,22 @@ def _jk_energy_per_atom(int3c2e_opt, dm, j_factor=1, k_factor=1, hermi=0,
     # assert cp.array_equal(aux_sorting, argsort_aux(l_ctr_aux_offsets, uniq_l_ctr_aux))
     ksh_offsets_cpu = l_ctr_aux_offsets
     ksh_offsets_gpu = cp.asarray(ksh_offsets_cpu+mol.nbas, dtype=np.int32)
-    l_ctr_aux_counts = l_ctr_aux_offsets[1:] - l_ctr_aux_offsets[:-1]
+    aux_offsets = aux_loc[ksh_offsets_cpu]
+    aux_batches = len(aux_offsets) - 1
 
     if j_factor != 0:
         dm = dm_factor_l.dot(dm_factor_r.T)
 
     int3c2e_envs = int3c2e_opt.int3c2e_envs
     kern = libvhf_rys.sum_ejk_int3c2e_ip1
-    l = np.arange(laux+1)
-    nf = (l + 1) * (l + 2) // 2
     aux0 = aux1 = 0
     buf = cp.empty((nao_pair*batch_size))
     buf1 = cp.empty((blksize, nao, nao))
     buf2 = cp.empty((blksize, nao, nao))
     ejk = cp.zeros((mol.natm, 3))
     ejk_aux = cp.zeros((mol.natm, 3))
-    for kbatch, lk, in enumerate(uniq_l_ctr_aux[:,0]):
-        naux_in_batch = nf[lk] * l_ctr_aux_counts[kbatch]
+    for kbatch in range(aux_batches):
+        naux_in_batch = aux_offsets[kbatch+1] - aux_offsets[kbatch]
         aux_ao_offset = aux_loc[ksh_offsets_cpu[kbatch]]
         compressed = ndarray((nao_pair, naux_in_batch), buffer=buf)
         for k0, k1 in lib.prange(0, naux_in_batch, blksize):

gpu4pyscf/df/hessian/tests/test_df_rhf_hessian.py
@@ -289,7 +289,7 @@ class KnownValues(unittest.TestCase):
         mol2 = mol1 + mol1
         np.random.seed(8)
         nao = mol2.nao
-        nocc = 5
+        nocc = 20
         mo_coeff = cp.array(np.random.rand(nao, nao) - .5)
         mo_occ = cp.zeros(nao)
         mo_occ[:nocc] = 2
@@ -297,7 +297,7 @@ class KnownValues(unittest.TestCase):
 
         ref = rhf_fast._get_veff(opt, mo_coeff, mo_occ, j_factor=1, k_factor=1)
 
-        with lib.temporary_env(rhf_fast, get_avail_mem=(lambda **kw: nao**2*3*mol2.natm*16)):
+        with lib.temporary_env(rhf_fast, get_avail_mem=(lambda **kw: nao**2*3*mol2.natm*20)):
             veff = rhf_fast._get_veff(opt, mo_coeff, mo_occ, j_factor=1, k_factor=1)
             assert abs(ref - veff).max() < 1e-9
 

gpu4pyscf/df/hessian/tests/test_df_uhf_hessian.py
@@ -224,7 +224,7 @@ class KnownValues(unittest.TestCase):
         opt = int3c2e.Int3c2eOpt(mol, auxmol).build()
         dm_rhf = tag_array(dm[0]+dm[1], mo_coeff=mo_coeff[0], mo_occ=mo_occ[0]*2)
         ref = rhf_fast._jk_energy_per_atom(opt, dm_rhf, j_factor=1, k_factor=1)
-        with lib.temporary_env(uhf_fast, get_avail_mem=(lambda **kw: 2000000)):
+        with lib.temporary_env(uhf_fast, get_avail_mem=(lambda **kw: 5000000)):
             ejk = uhf_fast._jk_energy_per_atom(opt, dm, j_factor=1, k_factor=1)
         assert abs(ejk.sum(axis=(0,1))).max().get() < 1e-9
         assert abs(ejk-ref).max().get() < 1e-8
@@ -329,7 +329,7 @@ class KnownValues(unittest.TestCase):
         opt = int3c2e.Int3c2eOpt(mol1, auxmol).build()
         ref = rhf_fast._get_veff(opt, mo_coeff[0], mo_occ[0]*2, j_factor=1, k_factor=1)
 
-        with lib.temporary_env(uhf_fast, get_avail_mem=(lambda **kw: nao**2*3*mol1.natm*24)):
+        with lib.temporary_env(uhf_fast, get_avail_mem=(lambda **kw: nao**2*3*mol1.natm*20)):
             veff = uhf_fast._get_veff(opt, mo_coeff, mo_occ, j_factor=1, k_factor=1)
             assert abs(ref - veff[0]).max() < 1e-9
             assert abs(ref - veff[1]).max() < 1e-9