8a0a4c7b981a2a3af1dd00ef3ff52a674cc3312a
henry.wang1 <henry.wang1@bytedance.com>
2026-07-08 14:08:51 +0800

    Fix the bad atomic radii assigned to ghost atom, which causes a bad Becke partition and a negative BSSE

--- gpu4pyscf/dft/radi.py
+++ gpu4pyscf/dft/radi.py
@@ -22,6 +22,7 @@ import cupy
 import pyscf
 from pyscf.data import radii
 from pyscf.data.elements import charge as elements_proton
+from pyscf.data.elements import _std_symbol_without_ghost
 
 BRAGG_RADII = radii.BRAGG
 COVALENT_RADII = radii.COVALENT
@@ -35,7 +36,7 @@ def treutler_atomic_radii_adjust(mol, atomic_radii):
 # i > j
 # fac(i,j) = \frac{1}{4} ( \frac{ra(j)}{ra(i)} - \frac{ra(i)}{ra(j)}
 # fac(j,i) = -fac(i,j)
-    charges = [elements_proton(x) for x in mol.elements]
+    charges = [elements_proton(_std_symbol_without_ghost(x)) for x in mol.elements]
     rad = cupy.sqrt(atomic_radii[charges]) + 1e-200
     rr = rad.reshape(-1,1) * (1./rad)
     a = .25 * (rr.T - rr)
@@ -55,7 +56,7 @@ def get_treutler_fac(mol, atomic_radii):
     # fac(i,j) = \frac{1}{4} ( \frac{ra(j)}{ra(i)} - \frac{ra(i)}{ra(j)}
     # fac(j,i) = -fac(i,j)
     '''
-    charges = [elements_proton(x) for x in mol.elements]
+    charges = [elements_proton(_std_symbol_without_ghost(x)) for x in mol.elements]
     #atomic_radii = cupy.asarray(atomic_radii[charges])
     rad = numpy.sqrt(atomic_radii[charges]) + 1e-200
     rr = rad.reshape(-1,1) * (1./rad)
@@ -65,7 +66,7 @@ def get_treutler_fac(mol, atomic_radii):
     return cupy.asarray(a)
 
 def get_becke_fac(mol, atomic_radii):
-    charges = [elements_proton(x) for x in mol.elements]
+    charges = [elements_proton(_std_symbol_without_ghost(x)) for x in mol.elements]
     atomic_radii = numpy.asarray(atomic_radii[charges])
     rad = atomic_radii[charges] + 1e-200
     rr = rad.reshape(-1,1) * (1./rad)
