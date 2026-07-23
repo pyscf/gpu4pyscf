Fix np.log underflow in extract_pgto_params

gpu4pyscf/gto/mole.py
@@ -368,12 +368,16 @@ def extract_pgto_params(mol, op='diffuse'):
         # A quick estimation for the radius that each primitive GTO decays to the
         # value smaller than the required precision
         # c * r2**(l/2) * ((2*l+1)/4*np.pi)**.5 * exp(-e*r2) ~ precision
-        r2 = np.log((2*l+1)/(4*np.pi)/precision**2 * c**2 * 1e2**l + 1e-200) / (2*e)
+        tmp = (2*l+1)/(4*np.pi)/precision**2 * c**2 * 1e2**l
+        tmp = np.where(tmp > 0, tmp, 1e-200)
+        r2 = np.log(tmp) / (2*e)
         idx = groupby(basis_id, r2, 'argmax')
     else:
         # A quick estimation for the resolution of planewaves that each
         # primitive GTO requires
-        ke = np.log(c**2 / precision * 50**l + 1e-200) * e
+        tmp = c**2 / precision * 50**l
+        tmp = np.where(tmp > 0, tmp, 1e-200)
+        ke = np.log(tmp) * e
         idx = groupby(basis_id, ke, 'argmax')
     return e[idx], c[idx]
 