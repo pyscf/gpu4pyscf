05183e62b657e4902c8575565000837ef078a5a9
Qiming Sun <qiming.sun@bytedance.com>
2026-08-09 10:27:44 -0700

    Add MGGA kernel

--- gpu4pyscf/lib/multigrid/multigrid_v3/eval_mgga_mat.cu
+++ gpu4pyscf/lib/multigrid/multigrid_v3/eval_mgga_mat.cu
@@ -27,28 +27,26 @@
 #define TILE            4
 #define WARP_SIZE       32
 #define THREADS         64
+#define NEGLIGIBLE      1e-18
 
 template <int LI, int LJ, int SLICE_SIZE_I, int SLICE_SIZE_J, bool is_non_orthogonal>
 __global__ static
-void eval_tau_kernel(double *density, double *tau, double *dm, PBCIntEnvVars envs,
-                     double *supmol_img_coords, double factor,
-                     int *shl_pair_offsets, int64_t *dressed_bas_ij_idx,
-                     int *grid_tile_index, int ntiles, int tiles_per_block,
-                     double a_dot_b, double a_dot_c, double b_dot_c,
-                     double da_squared, double db_squared, double dc_squared,
-                     int mesh_a, int mesh_b, int mesh_c)
+void eval_mgga_mat_kernel(double *out, double *vxc_weights, PBCIntEnvVars envs,
+                         double *supmol_img_coords,
+                         int *shl_pair_offsets, int64_t *dressed_bas_ij_idx,
+                         int *grid_tile_index, int ntiles, int tiles_per_block,
+                         double a_dot_b, double a_dot_c, double b_dot_c,
+                         double da_squared, double db_squared, double dc_squared,
+                         int mesh_a, int mesh_b, int mesh_c)
 {
     constexpr int threads = THREADS;
-    constexpr int WARPS = THREADS / WARP_SIZE;
     int thread_id = threadIdx.x;
     int tile_id0 = blockIdx.x * tiles_per_block;
     __shared__ int a_upper, b_upper, c_upper;
     __shared__ double start_position_x, start_position_y, start_position_z;
-    __shared__ double rho_value[TILE*TILE*TILE*WARPS];
-    __shared__ double tau_value[TILE*TILE*TILE*WARPS];
-
-    constexpr int nfi = (LI + 1) * (LI + 2) / 2;
-    constexpr int nfj = (LJ + 1) * (LJ + 2) / 2;
+    __shared__ double vrho_cache[TILE*TILE*TILE];
+    __shared__ double vtau_cache[TILE*TILE*TILE];
+    double *vtau_weights = vxc_weights + mesh_a * mesh_b * mesh_c;
 
 for (int tile_id = tile_id0; tile_id < min(tile_id0+tiles_per_block, ntiles); tile_id++) {
     int tile_index = grid_tile_index[tile_id];
@@ -64,6 +62,9 @@ for (int tile_id = tile_id0; tile_id < min(tile_id0+tiles_per_block, ntiles); ti
     int b_start = tile_b_index * TILE;
     int c_start = tile_c_index * TILE;
 
+    constexpr int nfi = (LI + 1) * (LI + 2) / 2;
+    constexpr int nfj = (LJ + 1) * (LJ + 2) / 2;
+
     int *bas = envs.bas;
     double *env = envs.env;
     int nbas = envs.nbas;
@@ -78,27 +79,24 @@ for (int tile_id = tile_id0; tile_id < min(tile_id0+tiles_per_block, ntiles); ti
         c_upper = min(c_start + TILE, mesh_c) - c_start;
     }
 
-    int lane = thread_id % WARP_SIZE;
-    int warp = thread_id / WARP_SIZE;
-    for (int n = thread_id; n < TILE*TILE*TILE*WARPS; n += threads) {
-        rho_value[n] = 0;
-        tau_value[n] = 0;
+    int a_idx = a_start + thread_id / (TILE*TILE);
+    int b_idx = b_start + thread_id / TILE % TILE;
+    int c_idx = c_start + thread_id % TILE;
+    if (a_idx < mesh_a && b_idx < mesh_b && c_idx < mesh_c) {
+        int abc_idx = (a_idx * mesh_b + b_idx) * mesh_c + c_idx;
+        vrho_cache[thread_id] = vxc_weights[abc_idx];
+        vtau_cache[thread_id] = vtau_weights[abc_idx];
     }
     __syncthreads();
 
-    for (int pair_id = shl_pair0+thread_id; pair_id < shl_pair1+thread_id; pair_id += threads) {
-        int ish = 0;
-        int jsh = 0;
-        if (pair_id < shl_pair1) {
-            int64_t bas_ij = dressed_bas_ij_idx[pair_id];
-            ish = bas_ij / NBAS_MAX;
-            jsh = bas_ij % NBAS_MAX;
-        }
+    for (int pair_id = shl_pair0+thread_id; pair_id < shl_pair1; pair_id += threads) {
+        int64_t bas_ij = dressed_bas_ij_idx[pair_id];
+        int ish = bas_ij / NBAS_MAX;
+        int jsh = bas_ij % NBAS_MAX;
         int latsum_idx = ish / nbas;
         ish = ish - nbas * latsum_idx;
         int jL = jsh / bvk_nbas;
         jsh = jsh - bvk_nbas * jL;
-        int jsh_cell0 = jsh % nbas;
         double Lx = supmol_img_coords[latsum_idx*3+0];
         double Ly = supmol_img_coords[latsum_idx*3+1];
         double Lz = supmol_img_coords[latsum_idx*3+2];
@@ -129,55 +127,38 @@ for (int tile_id = tile_id0; tile_id < min(tile_id0+tiles_per_block, ntiles); ti
         double z0 = start_position_z - zij;
         double gaussian_exponent_at_reference = aij * distance_squared(x0, y0, z0);
         double gaussian_starting_exponent = theta_ij * rr_ij + gaussian_exponent_at_reference;
-        double gaussian_starting_point = 0.;
-        double recursion_factor_a_start = 0.;
-        double recursion_factor_b_start = 0.;
-        double recursion_factor_c_start = 0.;
-        double exp_da_squared = 0.;
-        double exp_db_squared = 0.;
-        double exp_dc_squared = 0.;
-        double exp_dadb = 0.;
-        double exp_dadc = 0.;
-        double exp_dbdc = 0.;
-        if (gaussian_starting_exponent < 680.) {
-            double ci = env[bas[ish*BAS_SLOTS+PTR_COEFF]];
-            double cj = env[bas[jsh*BAS_SLOTS+PTR_COEFF]];
-            double cc = ci * cj;
-            gaussian_starting_point = exp(-gaussian_starting_exponent);
-            gaussian_starting_point *= factor * cc;
-            if (ish == jsh_cell0) {
-                gaussian_starting_point *= 0.5;
-            }
-            double cross_term_a = c_dxyz_dabc[0] * x0 + c_dxyz_dabc[1] * y0 + c_dxyz_dabc[2] * z0;
-            double cross_term_b = c_dxyz_dabc[3] * x0 + c_dxyz_dabc[4] * y0 + c_dxyz_dabc[5] * z0;
-            double cross_term_c = c_dxyz_dabc[6] * x0 + c_dxyz_dabc[7] * y0 + c_dxyz_dabc[8] * z0;
-            recursion_factor_a_start = exp(-aij * (2 * cross_term_a + da_squared));
-            recursion_factor_b_start = exp(-aij * (2 * cross_term_b + db_squared));
-            recursion_factor_c_start = exp(-aij * (2 * cross_term_c + dc_squared));
-            exp_da_squared = exp(-2 * aij * da_squared);
-            exp_db_squared = exp(-2 * aij * db_squared);
-            exp_dc_squared = exp(-2 * aij * dc_squared);
-            exp_dadb = exp(-2 * aij * a_dot_b);
-            exp_dadc = exp(-2 * aij * a_dot_c);
-            exp_dbdc = exp(-2 * aij * b_dot_c);
+        if (gaussian_starting_exponent > 680.) {
+            continue;
         }
+        double ci = env[bas[ish*BAS_SLOTS+PTR_COEFF]];
+        double cj = env[bas[jsh*BAS_SLOTS+PTR_COEFF]];
+        double cc = ci * cj;
+        int ish_cell0 = ish;
+        int jsh_cell0 = jsh % nbas;
+        if (ish_cell0 == jsh_cell0) {
+            cc *= .5;
+        }
+        double gaussian_starting_point = exp(-gaussian_starting_exponent) * cc;
+        double cross_term_a = c_dxyz_dabc[0] * x0 + c_dxyz_dabc[1] * y0 + c_dxyz_dabc[2] * z0;
+        double cross_term_b = c_dxyz_dabc[3] * x0 + c_dxyz_dabc[4] * y0 + c_dxyz_dabc[5] * z0;
+        double cross_term_c = c_dxyz_dabc[6] * x0 + c_dxyz_dabc[7] * y0 + c_dxyz_dabc[8] * z0;
+        double recursion_factor_a_start = exp(-aij * (2 * cross_term_a + da_squared));
+        double recursion_factor_b_start = exp(-aij * (2 * cross_term_b + db_squared));
+        double recursion_factor_c_start = exp(-aij * (2 * cross_term_c + dc_squared));
+        double exp_da_squared = exp(-2 * aij * da_squared);
+        double exp_db_squared = exp(-2 * aij * db_squared);
+        double exp_dc_squared = exp(-2 * aij * dc_squared);
+        double exp_dadb = exp(-2 * aij * a_dot_b);
+        double exp_dadc = exp(-2 * aij * a_dot_c);
+        double exp_dbdc = exp(-2 * aij * b_dot_c);
 #pragma unroll
         for (int dm_i0 = 0; dm_i0 < nfi; dm_i0 += SLICE_SIZE_I) {
 #pragma unroll
         for (int dm_j0 = 0; dm_j0 < nfj; dm_j0 += SLICE_SIZE_J) {
-            size_t nao = envs.ao_loc[bvk_nbas];
-            int i0 = envs.ao_loc[ish];
-            int j0 = envs.ao_loc[jsh];
-            double dm_cache[SLICE_SIZE_I * SLICE_SIZE_J];
-            if (pair_id < shl_pair1) {
+            double vj_cache[SLICE_SIZE_I * SLICE_SIZE_J];
 #pragma unroll
-                for (int i = 0; i < SLICE_SIZE_I; ++i) {
-                    if (SLICE_SIZE_I < nfi && dm_i0 + i > nfi) break;
-#pragma unroll
-                for (int j = 0; j < SLICE_SIZE_J; ++j) {
-                    if (SLICE_SIZE_J < nfj && dm_j0 + j > nfj) break;
-                    dm_cache[i*SLICE_SIZE_J+j] = dm[(i0+dm_i0+i)*nao+j0+dm_j0+j];
-                } }
+            for (int n = 0; n < SLICE_SIZE_I*SLICE_SIZE_J; ++n) {
+                vj_cache[n] = 0;
             }
 
             double x, y, z;
@@ -222,9 +203,7 @@ for (int tile_id = tile_id0; tile_id < min(tile_id0+tiles_per_block, ntiles); ti
                          gaussian_xyz *= recursion_factor_c,
                          recursion_factor_c *= exp_dc_squared) {
 
-                        double rho = 0;
-                        double val = 0;
-                        if (pair_id < shl_pair1 && fabs(gaussian_xyz) > 1e-18) {
+                        if (fabs(gaussian_xyz) > 1e-18) {
                             double i_cartesian[nfi];
                             double i_gradient[3*nfi];
                             gto_cartesian<LI>(i_cartesian, x - xi, y - yi, z - zi);
@@ -242,38 +221,26 @@ for (int tile_id = tile_id0; tile_id < min(tile_id0+tiles_per_block, ntiles); ti
                             rename_registers(j_gradient      , dm_j0, nfj, SLICE_SIZE_J);
                             rename_registers(j_gradient+nfj  , dm_j0, nfj, SLICE_SIZE_J);
                             rename_registers(j_gradient+nfj*2, dm_j0, nfj, SLICE_SIZE_J);
+
+                            int abc_index = a_index * TILE*TILE + b_index*TILE + c_index;
+                            double rho_fac = gaussian_xyz * vrho_cache[abc_index];
+                            double tau_fac = gaussian_xyz * vtau_cache[abc_index];
 #pragma unroll
                             for (int i = 0; i < SLICE_SIZE_I; ++i) {
                                 if (SLICE_SIZE_I < nfi && dm_i0 + i > nfi) break;
-                                double s0 = 0;
-                                double s1 = 0;
-                                double s2 = 0;
-                                double s3 = 0;
+                                double s0 = rho_fac * i_cartesian[i];
+                                double s1 = tau_fac * i_gradient[i];
+                                double s2 = tau_fac * i_gradient[i+nfi];
+                                double s3 = tau_fac * i_gradient[i+nfi*2];
 #pragma unroll
                                 for (int j = 0; j < SLICE_SIZE_J; ++j) {
                                     if (SLICE_SIZE_J < nfj && dm_j0 + j > nfj) break;
-                                    double dm_val = dm_cache[i * SLICE_SIZE_J + j];
-                                    s0 += dm_val * j_cartesian[j];
-                                    s1 += dm_val * j_gradient[j      ];
-                                    s2 += dm_val * j_gradient[j+nfj  ];
-                                    s3 += dm_val * j_gradient[j+nfj*2];
+                                    vj_cache[i*SLICE_SIZE_J+j] += s0 * j_cartesian[j];
+                                    vj_cache[i*SLICE_SIZE_J+j] += s1 * j_gradient[j];
+                                    vj_cache[i*SLICE_SIZE_J+j] += s2 * j_gradient[j+nfj];
+                                    vj_cache[i*SLICE_SIZE_J+j] += s3 * j_gradient[j+nfj*2];
                                 }
-                                rho += s0 * i_cartesian[i];
-                                val += s1 * i_gradient[i      ];
-                                val += s2 * i_gradient[i+nfi  ];
-                                val += s3 * i_gradient[i+nfi*2];
                             }
-                            rho *= gaussian_xyz;
-                            val *= gaussian_xyz;
-                        }
-                        for (int offset = WARP_SIZE/2; offset > 0; offset >>= 1) {
-                            rho += __shfl_down_sync(0xffffffff, rho, offset);
-                            val += __shfl_down_sync(0xffffffff, val, offset);
-                        }
-                        if (lane == 0) {
-                            int abc_index = a_index * TILE*TILE + b_index*TILE + c_index;
-                            rho_value[abc_index+TILE*TILE*TILE*warp] += rho;
-                            tau_value[abc_index+TILE*TILE*TILE*warp] += val;
                         }
                         if constexpr (is_non_orthogonal) {
                             x += c_dxyz_dabc[6];
@@ -296,44 +263,38 @@ for (int tile_id = tile_id0; tile_id < min(tile_id0+tiles_per_block, ntiles); ti
                     x += c_dxyz_dabc[0];
                 }
             }
+
+            size_t nao = envs.ao_loc[bvk_nbas];
+            int i0 = envs.ao_loc[ish];
+            int j0 = envs.ao_loc[jsh];
+#pragma unroll
+            for (int i = 0; i < SLICE_SIZE_I; ++i) {
+                if (SLICE_SIZE_I < nfi && dm_i0 + i > nfi) break;
+#pragma unroll
+            for (int j = 0; j < SLICE_SIZE_J; ++j) {
+                if (SLICE_SIZE_J < nfj && dm_j0 + j > nfj) break;
+                atomicAdd(out + (i0+dm_i0+i)*nao+j0+dm_j0+j, vj_cache[i*SLICE_SIZE_J+j]);
+            } }
         } }
     }
-    __syncthreads();
-
-    int a_idx = a_start + thread_id / (TILE*TILE);
-    int b_idx = b_start + thread_id / TILE % TILE;
-    int c_idx = c_start + thread_id % TILE;
-    if (a_idx < mesh_a && b_idx < mesh_b && c_idx < mesh_c) {
-        double rho = rho_value[thread_id];
-        double val = tau_value[thread_id];
-        for (int i = 1; i < WARPS; i++) {
-            rho += rho_value[thread_id+i*TILE*TILE*TILE];
-            val += tau_value[thread_id+i*TILE*TILE*TILE];
-        }
-        atomicAdd(density + (a_idx * mesh_b + b_idx) * mesh_c + c_idx, rho);
-        atomicAdd(tau + (a_idx * mesh_b + b_idx) * mesh_c + c_idx, val);
-    }
-    __syncthreads();
 }
 }
 
 extern "C" {
-#define eval_tau_kernel_case(li, lj, slice_i, slice_j, orth) \
+#define eval_mgga_mat_kernel_case(li, lj, slice_i, slice_j, orth) \
     case (li * LMAX1 + lj): \
-        eval_tau_kernel<li,lj,slice_i,slice_j,orth><<<block_grid, THREADS>>>( \
-            density, tau, dm, *envs, supmol_img_coords, factor, \
-            shl_pair_offsets, dressed_bas_ij_idx, \
+        eval_mgga_mat_kernel<li,lj,slice_i,slice_j,orth><<<block_grid, THREADS>>>( \
+            out, vxc_weights, *envs, supmol_img_coords, shl_pair_offsets, dressed_bas_ij_idx, \
             grid_tile_index, n_contributing_tiles, tiles_per_block, \
             a_dot_b, a_dot_c, b_dot_c, da_squared, db_squared, dc_squared, \
             mesh_a, mesh_b, mesh_c); \
     break
 
-int evaluate_tau(double *density, double *tau, double *dm, PBCIntEnvVars *envs,
-                 double *dxyz_dabc, double *supmol_img_coords,
-                 int i_angular, int j_angular, int tiles_per_block,
-                 int *shl_pair_offsets, int64_t *dressed_bas_ij_idx,
-                 int *grid_tile_index, int n_contributing_tiles, int *mesh,
-                 double factor)
+int evaluate_mgga_mat(double *out, double *vxc_weights, PBCIntEnvVars *envs,
+                     double *dxyz_dabc, double *supmol_img_coords,
+                     int i_angular, int j_angular, int tiles_per_block,
+                     int *shl_pair_offsets, int64_t *dressed_bas_ij_idx,
+                     int *grid_tile_index, int n_contributing_tiles, int *mesh)
 {
     int mesh_a = mesh[0];
     int mesh_b = mesh[1];
@@ -346,25 +307,25 @@ int evaluate_tau(double *density, double *tau, double *dm, PBCIntEnvVars *envs,
     double db_squared = distance_squared(dxyz_dabc[3], dxyz_dabc[4], dxyz_dabc[5]);
     double dc_squared = distance_squared(dxyz_dabc[6], dxyz_dabc[7], dxyz_dabc[8]);
     switch (i_angular * LMAX1 + j_angular) {
-        eval_tau_kernel_case(0,0, 1, 1, 0);
-        eval_tau_kernel_case(1,0, 3, 1, 0);
-        eval_tau_kernel_case(1,1, 3, 3, 0);
-        eval_tau_kernel_case(2,0, 6, 1, 0);
-        eval_tau_kernel_case(2,1, 6, 3, 0);
-        eval_tau_kernel_case(2,2, 6, 6, 0);
-        eval_tau_kernel_case(3,0,10, 1, 0);
-        eval_tau_kernel_case(3,1,10, 3, 0);
-        eval_tau_kernel_case(3,2, 5, 6, 0);
-        eval_tau_kernel_case(3,3,10, 5, 0);
-        eval_tau_kernel_case(4,0,15, 1, 0);
-        eval_tau_kernel_case(4,1,15, 3, 0);
-        eval_tau_kernel_case(4,2, 8, 6, 0);
-        eval_tau_kernel_case(4,3, 8, 5, 0);
-        eval_tau_kernel_case(4,4, 5, 5, 0);
+        eval_mgga_mat_kernel_case(0,0, 1, 1, 0);
+        eval_mgga_mat_kernel_case(1,0, 3, 1, 0);
+        eval_mgga_mat_kernel_case(1,1, 3, 3, 0);
+        eval_mgga_mat_kernel_case(2,0, 6, 1, 0);
+        eval_mgga_mat_kernel_case(2,1, 6, 3, 0);
+        eval_mgga_mat_kernel_case(2,2, 6, 6, 0);
+        eval_mgga_mat_kernel_case(3,0,10, 1, 0);
+        eval_mgga_mat_kernel_case(3,1,10, 3, 0);
+        eval_mgga_mat_kernel_case(3,2,10, 6, 0);
+        eval_mgga_mat_kernel_case(3,3, 5,10, 0);
+        eval_mgga_mat_kernel_case(4,0,15, 1, 0);
+        eval_mgga_mat_kernel_case(4,1,15, 3, 0);
+        eval_mgga_mat_kernel_case(4,2, 8, 6, 0);
+        eval_mgga_mat_kernel_case(4,3, 8, 5, 0);
+        eval_mgga_mat_kernel_case(4,4, 15, 3, 0);
     }
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess) {
-        fprintf(stderr, "CUDA Error in eval_tau_kernel: %s\n", cudaGetErrorString(err));
+        fprintf(stderr, "CUDA Error in eval_lda_mat_kernel: %s\n", cudaGetErrorString(err));
         return 1;
     }
     return 0;
