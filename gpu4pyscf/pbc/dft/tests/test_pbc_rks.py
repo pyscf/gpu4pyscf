5025fc4a1d3a9a41873ba07d6b3d550d5bca1139
Qiming Sun <qiming.sun@bytedance.com>
2026-08-21 08:01:16 -0700

    Fix int1e_r2_origi and adjust int1e integral accuracy (#857)
    * Fix int1e_r2_origi; Adjust int1e integral accuracy
    
    * Restore drivers/__init__.py
    
    * Update thresholds in tests
    
    * Update tests

--- gpu4pyscf/pbc/dft/tests/test_pbc_rks.py
+++ gpu4pyscf/pbc/dft/tests/test_pbc_rks.py
@@ -242,8 +242,8 @@ class KnownValues(unittest.TestCase):
         kpts_band = np.random.random((2,3))
         e0, c0 = mf_ref.get_bands(kpts_band)
         e1, c1 = kmf.get_bands(kpts_band)
-        self.assertAlmostEqual(abs(e1[0].get() - e0[0]).max(), 0, 6)
-        self.assertAlmostEqual(abs(e1[1].get() - e0[1]).max(), 0, 6)
+        self.assertAlmostEqual(abs(e1[0].get() - e0[0]).max(), 0, delta=1e-6)
+        self.assertAlmostEqual(abs(e1[1].get() - e0[1]).max(), 0, delta=1e-6)
 
     def test_kpts_gga_fft(self):
         cell = self.cell
