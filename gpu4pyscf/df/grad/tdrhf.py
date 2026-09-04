Fix OOM df-tdrhf gradients module (fix #881) (#885)
* Fix OOM df-tdrhf gradients module (fix #881)

gpu4pyscf/df/grad/tdrhf.py
@@ -430,7 +430,10 @@ def _jk_energies_by_dm_factors(int3c2e_opt, dm_factors, j_factor, k_factor,
 
     mem_free = get_avail_mem(exclude_memory_pool=True)
     mem_avail = mem_free - 2*naux*np.dot(dm1_noccs, dm2_noccs)*8 - 2*n_dm*nao**2*8
-    batch_size = int(mem_avail*.5/(n_dm*nao_pair*8))
+    if sum_results:
+        batch_size = int(mem_avail*.5/(2*nao_pair*8))
+    else:
+        batch_size = int(mem_avail*.5/(n_dm*nao_pair*8))
     laux = auxmol.uniq_l_ctr[:,0].max()
     if batch_size <= (laux+1)*(laux+2)//2:
         raise RuntimeError('Insufficient memory for storing intermediates')
@@ -560,7 +563,7 @@ def _jk_energies_by_dm_factors(int3c2e_opt, dm_factors, j_factor, k_factor,
     if sum_results:
         kern = libvhf_rys.sum_ejk_int3c2e_ip1
         ejk = cp.zeros((mol.natm, 3))
-        buf = cp.empty((nao_pair*batch_size))
+        buf, buf3 = cp.empty((2, nao_pair*batch_size))
     else:
         kern = libvhf_rys.ejk_int3c2e_ip1
         ejk = cp.zeros((n_dm, mol.natm, 3))
@@ -570,7 +573,8 @@ def _jk_energies_by_dm_factors(int3c2e_opt, dm_factors, j_factor, k_factor,
         naux_in_batch = nf[lk] * l_ctr_aux_counts[kbatch]
         aux_ao_offset = aux_loc[ksh_offsets_cpu[kbatch]]
         if sum_results:
-            compressed = cp.zeros((nao_pair, naux_in_batch))
+            compressed = ndarray((nao_pair, naux_in_batch), buffer=buf3)
+            compressed.fill(0.)
         else:
             compressed = ndarray((n_dm, nao_pair, naux_in_batch), buffer=buf)
         for k0, k1 in lib.prange(0, naux_in_batch, blksize):