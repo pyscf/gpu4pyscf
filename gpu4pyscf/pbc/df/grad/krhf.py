17b3ed4732080d45a6959b9a0c20bc7937ddca74
puzhichen.996 <puzhichen.996@bytedance.com>
2026-09-11 09:19:20 +0800

    fix the codes as ruff, delete the unused debugging lines

--- gpu4pyscf/pbc/df/grad/krhf.py
+++ gpu4pyscf/pbc/df/grad/krhf.py
@@ -352,9 +352,7 @@ def _get_ejk_derivatives(int3c2e_opt, dm, kpts=None, hermi=0, j_factor=1., k_fac
         nbatches_shl_pair = len(shl_pair_offsets) - 1
         aft_envs = ft_opt.aft_envs
         shm_size = aft_jk._estimate_max_shm_size(cell, (1, 0))
-        mem_avail = get_avail_mem(exclude_memory_pool=True)
-        Gblksize = _get_lr_block_size(
-            nao, nocc, naux, nkpts, nkpts_uniq, ngrids)
+        Gblksize = _get_lr_block_size(nao, nocc, naux, nkpts, nkpts_uniq, ngrids)
         log.debug1('bas_ij_idx=%d shm_size=%d blksize=%d',
                    len(bas_ij_idx), shm_size, Gblksize)
 
