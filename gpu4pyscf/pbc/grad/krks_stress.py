c7e6d0617fb30ba3e95d41ef557aedeb87712251
henry.wang1 <henry.wang1@bytedance.com>
2026-09-24 16:12:10 +0800

    KRKS LDA and GGA stress supported (with or without grid response)

--- gpu4pyscf/pbc/grad/krks_stress.py
+++ gpu4pyscf/pbc/grad/krks_stress.py
@@ -100,13 +100,6 @@ def get_vxc(ks_grad, cell, dm_kpts, kpts, with_j=False, with_nuc=False):
 
     assert kpts.ndim == 2
     assert dm_kpts.ndim == 3
-    if not cell.cart:
-        c2s = asarray(cell.cart2sph_coeff())
-        dm_kpts = sandwich_dot(dm_kpts, c2s.T)
-        # Ensure all AOs are evaluated in the Cartesian GTOs as ao_ks strain
-        # derivatives currently supports Cartesian format only
-        cell = cell.copy()
-        cell.cart = True
     nkpts, nao = dm_kpts.shape[:2]
     assert nkpts == len(kpts)
 
